32ddb794a89543999889b5d6220a9d94b241a2f0
CZ <chongzhe@192.168.1.3>
2022-07-30 00:27:18 +0800

    add more BSC metrics

--- src/processor.ts
+++ src/processor.ts
@@ -3,10 +3,14 @@ import { ERC20Context, ERC20Processor } from './types/erc20_processor'
 import { AnyswapERC20Context, AnyswapERC20Processor } from "./types/anyswaperc20_processor";
 import { AnyswapRouterContext, AnyswapRouterProcessor } from './types/anyswaprouter_processor';
 import { LogAnySwapInEvent, LogAnySwapOut_address_address_address_uint256_uint256_uint256_Event, LogAnySwapOut_address_address_string_uint256_uint256_uint256_Event } from './types/AnyswapRouter';
+import { BscAnyswapRouterContext, BscAnyswapRouterProcessor } from './types/bscanyswaprouter_processor';
+import { LogAnySwapInEvent as BscLogAnySwapInEvent, LogAnySwapOutEvent as BscLogAnySwapOutEvent } from './types/BscAnyswapRouter';
+import { Bep20Context, Bep20Processor } from './types/bep20_processor';
+
 // import { TransferEvent } from './types/ERC20'
 
 const startBlock = 14215845 
-const startBlock_BSC = 13312128 
+const startBlock_BSC = 13312128  
 const startBlock_Ropsten = 12549988
 
 // ETH addresses
@@ -15,7 +19,9 @@ const routerAddress = "0xba8da9dcf11b50b03fd5284f164ef5cdef910705"
 const wethAddress = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
 
 // BSC addresses
-const anyETHAddress_BSC = "0x6f817a0ce8f7640add3bc0c1c2298635043c2423"
+const anyETHAddress_BSC = "0xdebb1d6a2196f2335ad51fbde7ca587205889360"
+const routerAddress_BSC = "0xd1c5966f9f5ee6881ff6b261bbeda45972b1b5f3"
+const wethAddress_BSC = "0x2170ed0880ac9a755fd29b2688956bd959f933f8"
 
 // Ropsten addresses
 const MTT_address = "0x39e68dd41af6fd870f27a6b77cbcffa64626b0f3"
@@ -46,7 +52,6 @@ const handleSwapOut1 = async function (event: LogAnySwapOut_address_address_addr
   const outAmount = Number(event.args.amount.toBigInt() / BigInt(10 ** 12)) / (10**6)
   ctx.meter.Counter('anyswapOutTotal').add(outAmount)
   ctx.meter.Counter('netSwapFlow').add(outAmount)
-
 }
 
 const handleSwapOut2 = async function (event: LogAnySwapOut_address_address_string_uint256_uint256_uint256_Event, ctx: AnyswapRouterContext) {
@@ -55,13 +60,35 @@ const handleSwapOut2 = async function (event: LogAnySwapOut_address_address_stri
   ctx.meter.Counter('netSwapFlow').add(outAmount)
 }
 
-const anyEthTotalSupplyBscProcessor = async function (_: any, ctx: AnyswapERC20Context) {
-  const totalSupplyBSC = Number((await ctx.contract.totalSupply()).toBigInt() / BigInt(10 ** 12)) / (10**6)
+// BSC handlers
+var TOTAL_SUPPLY_BSC: number
+const anyEthTotalSupplyProcessorBSC = async function (_: any, ctx: Bep20Context) {
+  TOTAL_SUPPLY_BSC = Number((await ctx.contract.totalSupply()).toBigInt() / BigInt(10 ** 12)) / (10**6)
   
-  ctx.meter.Histogram('anyETH_bsc_total_supply').record(totalSupplyBSC)
+  ctx.meter.Histogram('anyETH_bsc_total_supply').record(TOTAL_SUPPLY_BSC)
 }
 
 //netBalance is weth_balance - anyswap balance
+const wethBalanceProcessorBSC = async function (block: any, ctx: Bep20Context) {
+  const balance = Number((await ctx.contract.balanceOf(anyEthAddress)).toBigInt() / BigInt(10 ** 12)) / (10**6)
+  ctx.meter.Histogram('weth_bsc_balance').record(balance)
+  ctx.meter.Histogram('netBalance_bsc').record(balance - TOTAL_SUPPLY_BSC)
+}
+
+// netSwapFlow is defined as all anyswapOut events - anyswapIn events
+const handleSwapInBSC = async function (event: BscLogAnySwapInEvent, ctx: BscAnyswapRouterContext) {
+  const inAmount = Number(event.args.amount.toBigInt() / BigInt(10 ** 12)) / (10**6)
+  ctx.meter.Counter('anyswapInTotal_bsc').add(inAmount)
+  ctx.meter.Counter('netSwapFlow_bsc').sub(inAmount)
+}
+
+const handleSwapOutBSC = async function (event: BscLogAnySwapOutEvent, ctx: BscAnyswapRouterContext) {
+  const outAmount = Number(event.args.amount.toBigInt() / BigInt(10 ** 12)) / (10**6)
+  ctx.meter.Counter('anyswapOutTotal_bsc').add(outAmount)
+  ctx.meter.Counter('netSwapFlow_bsc').add(outAmount)
+}
+
+// Rospten handlers
 const mttBalanceProcessor = async function (block: any, ctx: ERC20Context) {
   const balance = Number((await ctx.contract.balanceOf(pool_address)).toBigInt() / BigInt(10 ** 12)) / (10**6)
   ctx.meter.Histogram('mtt_balance').record(balance)
@@ -71,6 +98,9 @@ const inFilter = AnyswapRouterProcessor.filters.LogAnySwapIn(null, anyEthAddress
 const outFilter1 = AnyswapRouterProcessor.filters['LogAnySwapOut(address,address,address,uint256,uint256,uint256)'](anyEthAddress)
 const outFilter2 = AnyswapRouterProcessor.filters['LogAnySwapOut(address,address,string,uint256,uint256,uint256)'](anyEthAddress)
 
+//BSC filters
+const inFilterBSC = BscAnyswapRouterProcessor.filters.LogAnySwapIn(null, anyETHAddress_BSC)
+const outFilterBSC = BscAnyswapRouterProcessor.filters.LogAnySwapOut(anyETHAddress_BSC)
 
 AnyswapERC20Processor.bind(anyEthAddress)
 .startBlock(startBlock)
@@ -86,10 +116,22 @@ AnyswapRouterProcessor.bind(routerAddress)
 .onLogAnySwapOut_address_address_address_uint256_uint256_uint256_(handleSwapOut1,outFilter1)
 .onLogAnySwapOut_address_address_string_uint256_uint256_uint256_(handleSwapOut2, outFilter2)
 
-// ERC20Processor.bind(anyETHAddress_BSC, 56)
-// .startBlock(startBlock_BSC)
-// .onBlock(anyEthTotalSupplyBscProcessor)
+// BSC processors
+Bep20Processor.bind(anyETHAddress_BSC, 56)
+.startBlock(startBlock_BSC)
+.onBlock(anyEthTotalSupplyProcessorBSC)
 
+Bep20Processor.bind(wethAddress_BSC, 56)
+.startBlock(startBlock_BSC)
+.onBlock(wethBalanceProcessorBSC)
+
+BscAnyswapRouterProcessor.bind(routerAddress_BSC, 56)
+.startBlock(startBlock_BSC)
+.onLogAnySwapIn(handleSwapInBSC, inFilterBSC)
+.onLogAnySwapOut(handleSwapOutBSC, outFilterBSC)
+
+
+//Rospten processors
 ERC20Processor.bind(MTT_address, 3)
 .startBlock(startBlock_Ropsten)
 .onBlock(mttBalanceProcessor)
