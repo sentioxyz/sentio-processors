13f295a33bde7439d225a90cf500d99c6671f058
CZ <chongzhe@192.168.1.4>
2022-08-23 11:58:07 +0800

    add initial dydx

---
+++ dydx/src/abiFormatConverter.js
@@ -0,0 +1,10 @@
+const ethers = require('ethers')
+humanReadableAbi =  [
+    "event LogDeposit(address depositorEthKey, uint256 starkKey, uint256 vaultId, uint256 assetType, uint256 nonQuantizedAmount, uint256 quantizedAmount)",
+    "event LogWithdrawalPerformed(uint256 ownerKey, uint256 assetType, uint256 nonQuantizedAmount, uint256 quantizedAmount, address recipient)",
+    "event LogMintWithdrawalPerformed(uint256 ownerKey, uint256 assetType, uint256 nonQuantizedAmount, uint256 quantizedAmount, uint256 assetId)",
+  ]
+const iface = new ethers.utils.Interface(humanReadableAbi);
+jsonAbi = iface.format(ethers.utils.FormatTypes.json);
+
+console.log(JSON.stringify(JSON.parse(jsonAbi), null, 2))
---
+++ dydx/src/processor.ts
@@ -0,0 +1,42 @@
+import { DYDX_PERPETUAL_ADDR, DYDX_V2_STARTBLOCK, USDC_ADDR} from './constant'
+import { LogDepositEvent, LogWithdrawalPerformedEvent, LogMintWithdrawalPerformedEvent } from './types/DydxPerpetual'
+import { DydxPerpetualContext, DydxPerpetualProcessor } from './types/dydxperpetual_processor'
+import { ERC20BalanceContext, ERC20BalanceProcessor } from './types/erc20balance_processor'
+
+// another flexible processing example:
+// https://github.com/NethermindEth/Forta-Agents/blob/main/DYDX-Bots/Perpetual-Large-Deposits-Withdrawals/src/agent.ts#L48
+const logDepositEventHandler =async (event:LogDepositEvent, ctx: DydxPerpetualContext) => {
+  // deducted 10^6 from this tx and log amount
+  //https://etherscan.io/tx/0xae3f3d6aaf8a63c5dab6780a811f1e6b514e193381ea6d2c1b2f245821358d5a
+  const amount = Number(event.args.quantizedAmount.toBigInt()) / 10 ** 6
+  ctx.meter.Gauge('deposit').record(amount)
+}
+
+// feature request:
+// get data directly from contract storage
+// https://github.com/NethermindEth/Forta-Agents/blob/main/DYDX-Bots/Perpetual-Large-Deposits-Withdrawals/src/token.address.fetcher.ts#L30
+// related info: https://docs.starkware.co/starkex-v4/starkex-deep-dive/starkex-specific-concepts#assetinfo-assettype-and-assetid 
+const logWithdrawalPerformedEventHandler =async (event:LogWithdrawalPerformedEvent, ctx: DydxPerpetualContext) => {
+  const amount = Number(event.args.quantizedAmount.toBigInt())  / 10 ** 6
+  ctx.meter.Gauge('withdrawl').record(amount)
+}
+
+const logMintWithdrawalPerformedEventHandler =async (event:LogMintWithdrawalPerformedEvent, ctx: DydxPerpetualContext) => {
+  const amount = Number(event.args.quantizedAmount.toBigInt() )/ 10 ** 6
+  ctx.meter.Gauge('mintWithdrawl').record(amount)
+}
+
+// USDC uses 6 for decimal
+const balanceProcessor = async function (block: any, ctx: ERC20BalanceContext) {
+  const balance = Number((await ctx.contract.balanceOf(DYDX_PERPETUAL_ADDR)).toBigInt() ) / 10 ** 6
+  ctx.meter.Gauge('usdc_balance').record(balance)
+}
+
+ERC20BalanceProcessor.bind({address: USDC_ADDR, startBlock: DYDX_V2_STARTBLOCK})
+.onBlock(balanceProcessor)
+
+//TODO: change starting block
+DydxPerpetualProcessor.bind({address: DYDX_PERPETUAL_ADDR, startBlock: 13424545})
+.onLogDeposit(logDepositEventHandler)
+.onLogMintWithdrawalPerformed(logMintWithdrawalPerformedEventHandler)
+.onLogWithdrawalPerformed(logWithdrawalPerformedEventHandler)
