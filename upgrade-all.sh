7c7e976544e36e3d06a56e5783b6082c51b4e221
Fuyao Zhao <zfy0701@gmail.com>
2022-08-24 22:09:52 -0700

    add upgrade all script, build all will also implicat upgrade all

--- upgrade-all.sh
+++ upgrade-all.sh
@@ -6,7 +6,7 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 for dir in $BASEDIR/*/; do # list directories in the form "/tmp/dirname/"
   dir=${dir%*/}                           # remove the trailing "/"
-  echo "### Build ${dir##*/}"             # print everything after the final "/"
+  echo "### Upgrading ${dir##*/}"             # print everything after the final "/"
   cd $dir
-  yarn install && yarn upgrade @sentio/processor && yarn sentio build
+  yarn install && yarn upgrade @sentio/processor
 done
