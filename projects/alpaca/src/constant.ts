61ea6ab3988e82d7320893c5ecad582aefd2a844
CZ <chongzhe@192.168.1.4>
2022-08-29 22:47:21 +0800

    initial commit for alpaca

---
+++ projects/alpaca/src/constant.ts
@@ -0,0 +1,8 @@
+export const VAULTS_THRESHOLD_MAP: Map<string, string> = new Map([
+  ["0x7C9e73d4C71dae564d41F78d56439bB4ba87592f".toLowerCase(), "BUSD"], // 100,000 BUSD
+  ["0xbfF4a34A4644a113E8200D7F1D79b3555f723AfE".toLowerCase(), "ETH"],     // 30 ETH
+  ["0x08FC9Ba2cAc74742177e0afC3dC8Aed6961c24e7".toLowerCase(), "BTCB"],      // 3 BTCB
+  ["0x158da805682bdc8ee32d52833ad41e74bb951e59".toLowerCase(), "USDT"], // 100,000 USDT
+  ["0xf1bE8ecC990cBcb90e166b71E368299f0116d421".toLowerCase(), "ALPACA"], // 200,000 ALPACA
+  ["0x3282d2a151ca00BfE7ed17Aa16E42880248CD3Cd".toLowerCase(), "TUSD"]  // 100,000 TUSD
+])  
---
+++ projects/alpaca/src/processor.ts
@@ -0,0 +1,19 @@
+import {VAULTS_THRESHOLD_MAP} from './constant'
+import { WorkEvent } from './types/vault'
+import { VaultProcessor, VaultContext, getVaultContract } from './types/vault'
+import type {BigNumber} from 'ethers'
+import {BigNumber as BN} from "bignumber.js";
+
+export const toBn = (ethersBn: BigNumber | BigInt) => new BN(ethersBn.toString());
+
+
+const workHandler = async function(event: WorkEvent, ctx: VaultContext) {
+  const amount = event.args.loan
+  const token = event.address
+  ctx.meter.Gauge('loan_amount').record(amount, {token: VAULTS_THRESHOLD_MAP.get(token)!})
+}
+
+for(let address of VAULTS_THRESHOLD_MAP.keys()) {
+  VaultProcessor.bind({address: address, network: 56}).onWork(workHandler)
+}
+
