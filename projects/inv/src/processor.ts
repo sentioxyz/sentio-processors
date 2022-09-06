6054f2560f3062485f27b50213b41d56684ba883
CZ <chongzhe@CZs-MacBook-Pro.local>
2022-09-06 08:24:47 +0800

    initial inverse finance commit

---
+++ projects/inv/src/processor.ts
@@ -0,0 +1,14 @@
+import { InvFeedProcessor, InvFeedContext } from './types/invfeed'
+
+const INV_ADDRESS = "0x210aC53b27f16e20A9aa7d16260F84693390258F".toLowerCase()
+
+const latestAnswerProcessor = async function (_:any, ctx: InvFeedContext) {
+  const latestAnswer = Number((await ctx.contract.latestAnswer()).toBigInt() / 10n ** 12n) / (10**6)
+  ctx.meter.Gauge('Inv_latestAnswer').record(latestAnswer)
+}
+
+InvFeedProcessor.bind({address: INV_ADDRESS, startBlock: 12415560})
+.onBlock(latestAnswerProcessor)
+
+
+
