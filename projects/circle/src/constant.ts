e26dc952acb3bae8d7f37429d7e0fd22d860a23e
CZ <chongzhe@CZs-MacBook-Pro.local>
2022-09-23 11:22:39 +0800

    inital circle

---
+++ projects/circle/src/constant.ts
@@ -0,0 +1,5 @@
+export const USDC_PROXY = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
+export const USDC_IMPLEMENTATION = "0xa2327a938febf5fec13bacfb16ae10ecbc4cbdcf"
+
+export const EUROC_PROXY = "0x1aBaEA1f7C830bD89Acc67eC4af516284b1bC33c"
+export const EUROC_IMPLEMENTATION = "0xa2327a938febf5fec13bacfb16ae10ecbc4cbdcf"
