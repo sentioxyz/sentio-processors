4f5867544c1b1fd7a0a86870dc281ee95c1859ca
CZ <chongzhe@CZs-MacBook-Pro.local>
2022-09-29 01:29:02 +0800

    add musd support for mystiko

--- projects/mystiko/src/processor.ts
+++ projects/mystiko/src/processor.ts
@@ -1,21 +1,37 @@
 import { toBigDecimal } from '@sentio/sdk/lib/utils'
 import { ERC20Context, ERC20Processor } from '@sentio/sdk/lib/builtin/erc20'
-import { COMMITMENT_POOL_GOERLI, MTT_GOERLI } from './constant'
+import { COMMITMENT_POOL_GOERLI_MTT, COMMITMENT_POOL_GOERLI_MUSD, MTT_GOERLI, MUSD_GOERLI } from './constant'
 import { CommitmentPoolContext, CommitmentPoolProcessor, CommitmentQueuedEvent } from './types/commitmentpool'
+import { BigDecimal } from '@sentio/sdk';
 
 const mttBalanceProcessor = async function (block: any, ctx: ERC20Context) {
-  const balance = toBigDecimal((await ctx.contract.balanceOf(COMMITMENT_POOL_GOERLI))).div(10**18)
+  const balance = toBigDecimal((await ctx.contract.balanceOf(COMMITMENT_POOL_GOERLI_MTT))).div(BigDecimal(10).pow(18))
   ctx.meter.Gauge('mtt_balance').record(balance)
 }
 
-const commitmentQueued = async function (event: CommitmentQueuedEvent, ctx: CommitmentPoolContext) {
-  const rollUpFee = event.args.rollupFee.div(10**18)
-  ctx.meter.Gauge('rollup_fee').record(rollUpFee)
+const mUsdBalanceProcessor = async function (block: any, ctx: ERC20Context) {
+  const balance = toBigDecimal((await ctx.contract.balanceOf(COMMITMENT_POOL_GOERLI_MUSD)))
+  ctx.meter.Gauge('musd_balance').record(balance)
+}
 
+const commitmentQueuedMtt = async function (event: CommitmentQueuedEvent, ctx: CommitmentPoolContext) {
+  const rollUpFee = toBigDecimal(event.args.rollupFee).div(BigDecimal(10).pow(18))
+  ctx.meter.Gauge('rollup_fee_mtt').record(rollUpFee)
+}
+
+const commitmentQueuedMusd = async function (event: CommitmentQueuedEvent, ctx: CommitmentPoolContext) {
+  const rollUpFee = toBigDecimal(event.args.rollupFee)
+  ctx.meter.Gauge('rollup_fee_musd').record(rollUpFee)
 }
 
 ERC20Processor.bind({address: MTT_GOERLI, network: 5})
 .onBlock(mttBalanceProcessor)
 
-CommitmentPoolProcessor.bind({address: COMMITMENT_POOL_GOERLI, network: 5})
-.onEventCommitmentQueued(commitmentQueued)
+CommitmentPoolProcessor.bind({address: COMMITMENT_POOL_GOERLI_MTT, network: 5})
+.onEventCommitmentQueued(commitmentQueuedMtt)
+
+ERC20Processor.bind({address: MUSD_GOERLI, network: 5})
+.onBlock(mUsdBalanceProcessor)
+
+CommitmentPoolProcessor.bind({address: COMMITMENT_POOL_GOERLI_MUSD, network: 5})
+.onEventCommitmentQueued(commitmentQueuedMusd)
