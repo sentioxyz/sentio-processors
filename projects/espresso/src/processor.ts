495a36d4da8d751d31a099718be0965906771f3f
Fuyao Zhao <zfy0701@gmail.com>
2022-10-12 21:21:25 -0700

    fix espresso decoding

--- projects/espresso/src/processor.ts
+++ projects/espresso/src/processor.ts
@@ -17,13 +17,25 @@ const handleBlockCommittedEvent = async (event: BlockCommittedEvent, ctx: CapeCo
     ctx.meter.Counter("note_type_unit_gt_one").add(1)
   } else if (note_types_uint.length == 1) {
     const note_types = note_types_uint[0]
-    switch(note_types) {
-      case 0: ctx.meter.Gauge("Transfer").record(1); break;
-      case 1: ctx.meter.Gauge("Mint").record(1); break;
-      case 2: ctx.meter.Gauge("Freeze").record(1); break;
-      case 3: ctx.meter.Gauge("Burn").record(1); break;
-      default: ctx.meter.Gauge("illegal_note_type").record(1); break;
-    } 
+    if (note_types) {
+      switch (note_types[0]) {
+        case 0:
+          ctx.meter.Gauge("Transfer").record(1);
+          break;
+        case 1:
+          ctx.meter.Gauge("Mint").record(1);
+          break;
+        case 2:
+          ctx.meter.Gauge("Freeze").record(1);
+          break;
+        case 3:
+          ctx.meter.Gauge("Burn").record(1);
+          break;
+        default:
+          ctx.meter.Gauge("illegal_note_type").record(1);
+          break;
+      }
+    }
   }
   else {
     ctx.meter.Gauge("Empty").record(1)
