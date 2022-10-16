a2c44fbc6fc09a924671f57df486a8840f6e828a
Fuyao Zhao <zfy0701@gmail.com>
2022-10-15 20:27:50 -0700

    sparse for ens

--- projects/ens/src/processor.ts
+++ projects/ens/src/processor.ts
@@ -1,14 +1,18 @@
 import { ERC20Context, ERC20Processor, TransferEvent } from '@sentio/sdk/lib/builtin/erc20'
 import { OldRegistarContext, OldRegistarProcessor, HashRegisteredEvent } from './types/oldregistar'
+import { Counter } from '@sentio/sdk/lib/core/meter'
+
+const registered = new Counter("registered", { sparse: true })
 
 const OLD_REGISTAR = "0x6090a6e47849629b7245dfa1ca21d94cd15878ef"
 
 async function hashRegisteredHandler(event: HashRegisteredEvent, ctx: OldRegistarContext) {
   const owner = event.args.owner
-  ctx.meter.Counter("registered").add(1, {owner: owner})
+  registered.add(ctx, 1, {owner: owner})
+  // ctx.meter.Counter("registered").add(1, {owner: owner})
   ctx.meter.Counter("total_registered").add(1)
 }
 
 
 OldRegistarProcessor.bind({address: OLD_REGISTAR})
-.onEventHashRegistered(hashRegisteredHandler)
+  .onEventHashRegistered(hashRegisteredHandler)
