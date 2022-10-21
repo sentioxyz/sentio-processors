1f3ee2a21d5bc8ff750a48055ef60a8918ff39be
Fuyao Zhao <zfy0701@gmail.com>
2022-10-21 08:52:59 -0700

    filter uncessary coins

--- projects/liquidswap/src/processor.ts
+++ projects/liquidswap/src/processor.ts
@@ -1,66 +1,77 @@
 import { liquidity_pool } from './types/aptos/liquidswap'
-import { aptos, Counter } from "@sentio/sdk";
+import { aptos, Counter, Gauge } from "@sentio/sdk";
 import { caculateValueInUsd, getCoinInfo, scaleDown } from "./utils";
 import { coin } from "@sentio/sdk/lib/builtin/aptos/0x1";
-import CoinInfo = coin.CoinInfo;
 
 const valueCounter = new Counter("value")
-const volumeCounter = new Counter("volume")
+const amountCounter = new Counter("amount")
+const volumeGauge = new Gauge("vol")
 
-liquidity_pool.bind()
+liquidity_pool.bind({startVersion: 0})
   .onEventPoolCreatedEvent(async (evt, ctx) => {
-    evt.type_arguments
-    const [x, y, pair] = await getCoinAndPairNames(evt.type_arguments)
-    ctx.meter.Counter("number_pool").add(1, { pair })
+    const pool = await getPairNames(evt.type_arguments)
+    ctx.meter.Counter("number_pool").add(1, { pool: pool })
   })
   .onEventLiquidityAddedEvent(async (evt, ctx) => {
-    const [x, y, pair] = await getCoinAndPairNames(evt.type_arguments)
+    const pool = await getPairNames(evt.type_arguments)
     const timestamp = ctx.transaction.timestamp
 
-    await addFor(ctx, x, evt.data_typed.added_x_val, timestamp, pair)
-    await addFor(ctx, y, evt.data_typed.added_y_val, timestamp, pair)
+    await addFor(ctx, evt.type_arguments[0], evt.data_typed.added_x_val, timestamp, pool)
+    await addFor(ctx, evt.type_arguments[1], evt.data_typed.added_y_val, timestamp, pool)
   })
   .onEventLiquidityRemovedEvent(async (evt, ctx) => {
-    const [x, y, pair] = await getCoinAndPairNames(evt.type_arguments)
+    const  pool = await getPairNames(evt.type_arguments)
     const timestamp = ctx.transaction.timestamp
 
-    await subFor(ctx, x, evt.data_typed.returned_x_val, timestamp, pair)
-    await subFor(ctx, y, evt.data_typed.returned_y_val, timestamp, pair)
+    await subFor(ctx, evt.type_arguments[0], evt.data_typed.returned_x_val, timestamp, pool)
+    await subFor(ctx, evt.type_arguments[1], evt.data_typed.returned_y_val, timestamp, pool)
   })
   .onEventSwapEvent(async (evt, ctx) => {
-    const [x, y, pair] = await getCoinAndPairNames(evt.type_arguments)
+    const pool = await getPairNames(evt.type_arguments)
     const timestamp = ctx.transaction.timestamp
 
-    await addFor(ctx, x, evt.data_typed.x_in, timestamp, pair)
-    await addFor(ctx, y, evt.data_typed.y_in, timestamp, pair)
-    await subFor(ctx, x, evt.data_typed.x_out, timestamp, pair)
-    await subFor(ctx, y, evt.data_typed.y_out, timestamp, pair)
+    await addFor(ctx, evt.type_arguments[0], evt.data_typed.x_in, timestamp, pool, true)
+    await addFor(ctx, evt.type_arguments[1], evt.data_typed.y_in, timestamp, pool, true)
+    await subFor(ctx, evt.type_arguments[0], evt.data_typed.x_out, timestamp, pool)
+    await subFor(ctx, evt.type_arguments[1], evt.data_typed.y_out, timestamp, pool)
     ctx.meter.Counter("number_swap").add(1)
   })
   .onEventFlashloanEvent(async (evt, ctx) => {
-    const [x, y, pair] = await getCoinAndPairNames(evt.type_arguments)
+    const pool = await getPairNames(evt.type_arguments)
     const timestamp = ctx.transaction.timestamp
 
-    await addFor(ctx, x, evt.data_typed.x_in, timestamp, pair)
-    await addFor(ctx, y, evt.data_typed.y_in, timestamp, pair)
-    await subFor(ctx, x, evt.data_typed.x_out, timestamp, pair)
-    await subFor(ctx, y, evt.data_typed.y_out, timestamp, pair)
+    await addFor(ctx, evt.type_arguments[0], evt.data_typed.x_in, timestamp, pool, true)
+    await addFor(ctx, evt.type_arguments[1], evt.data_typed.y_in, timestamp, pool, true)
+    await subFor(ctx, evt.type_arguments[0], evt.data_typed.x_out, timestamp, pool)
+    await subFor(ctx, evt.type_arguments[1], evt.data_typed.y_out, timestamp, pool)
+
     ctx.meter.Counter("number_flashloan").add(1)
   })
 
-async function addFor(ctx: aptos.AptosContext, coin: CoinInfo<any>, amount: bigint, timestamp: string, pair: string) {
-  valueCounter.add(ctx, await caculateValueInUsd(amount, coin, timestamp), { coin: coin.symbol, pair })
-  volumeCounter.add(ctx, amount, { coin: coin.symbol, pair })
+async function addFor(ctx: aptos.AptosContext, type: string, amount: bigint, timestamp: string, pool: string, recordTrading = false) {
+  const coin = await getCoinInfo(type)
+  const value = await caculateValueInUsd(amount, coin, timestamp)
+  valueCounter.add(ctx, value, { coin: coin.symbol, pool: pool })
+  amountCounter.add(ctx, scaleDown(amount, coin.decimals) , { coin: coin.symbol, pool: pool })
+  if (recordTrading) {
+    volumeGauge.record(ctx, value, { coin: coin.symbol, pool: pool })
+  }
 }
 
-async function subFor(ctx: aptos.AptosContext, coin: CoinInfo<any>, amount: bigint, timestamp: string, pair: string) {
-  valueCounter.sub(ctx, await caculateValueInUsd(amount, coin, timestamp), { coin: coin.symbol, pair })
-  volumeCounter.sub(ctx, amount, { coin: coin.symbol, pair })
+async function subFor(ctx: aptos.AptosContext, type: string, amount: bigint, timestamp: string, pool: string) {
+  const coin = await getCoinInfo(type)
+  const value = await caculateValueInUsd(amount, coin, timestamp)
+  valueCounter.sub(ctx, value, { coin: coin.symbol, pool: pool })
+  amountCounter.sub(ctx, scaleDown(amount, coin.decimals), { coin: coin.symbol, pool: pool })
 }
 
-async function getCoinAndPairNames(coins: string[]):Promise<[CoinInfo<any>,CoinInfo<any>, string]> {
+async function getPairNames(coins: [string, string, string]): Promise<string> {
   const coinx = await getCoinInfo(coins[0])
   const coiny = await getCoinInfo(coins[1])
-  const pair = `${coinx.symbol}-${coiny.symbol}`
-  return [coinx, coiny, pair]
+  const token = coins[2].includes("curves::Stable") ? "S" : "U"
+  // const xfullname = coins[0].split("::").slice(1).join("::")
+  // const yfullname = coins[1].split("::").slice(1).join("::")
+  const pool = `${coinx.symbol}-${coiny.symbol}-${token}`
+  return pool
+   // return [coinx, coiny, pool]
 }
--- projects/liquidswap/src/utils.ts
+++ projects/liquidswap/src/utils.ts
@@ -11,33 +11,42 @@ const CoinGeckoClient = new CoinGecko();
 
 const client = new AptosClient("https://mainnet.aptoslabs.com/")
 
-const coinInfos = new Map<string, CoinInfo<any>>()
+const coinInfos = new Map<string, ExtendedCoinInfo>()
 
-export async function getCoinInfo(type: string): Promise<CoinInfo<any>> {
-  let info = coinInfos.get(type)
-  if (!info) {
+// 0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa
+const whitelisted = new Set(["APT", "USDC", "USDT", "WETH" ]) // "DOGE", "UNI", "SHIBA"
+
+interface ExtendedCoinInfo extends CoinInfo<any> {
+  type: string
+}
+
+export async function getCoinInfo(type: string): Promise<ExtendedCoinInfo> {
+  let extInfo = coinInfos.get(type)
+  if (!extInfo) {
     const parts = type.split("::")
     const account = parts[0]
     try {
       const resource = await client.getAccountResource(account, `0x1::coin::CoinInfo<${type}>`)
-      info = resource.data as CoinInfo<any>
+      extInfo = { ...resource.data as CoinInfo<any>, type }
     } catch (e) {
-      info = {
+      extInfo = {
         name: parts[2],
         symbol: parts[2],
         decimals: 0,
-        supply: {vec:""}
+        supply: {vec:""},
+        type
       }
     }
-    coinInfos.set(type, info)
+    coinInfos.set(type, extInfo)
   }
-  return info
+  return extInfo!
 }
 
 export function scaleDown(n: bigint, decimal: number) {
-  if (decimal === 0) {
-    decimal = 1
-  }
+  // if (decimal === 0) {
+  //   throw Error("decimal become 0")
+  //   // decimal = 1
+  // }
   return new BigDecimal(n.toString()).div(new BigDecimal(10).pow(decimal))
 }
 
@@ -49,11 +58,26 @@ for (const e of csvjson) {
 
 const priceCache = new Map<string, number>()
 
-export async function getPrice(symbol: string, timestamp: string) {
+export async function getPrice(coin: ExtendedCoinInfo, timestamp: string) {
+  const symbol = coin.symbol
   const id = symbolToId.get(symbol)
   if (!id) {
+    // console.warn("price api doesn't include", symbol)
+    if (symbol.includes("USD")) {
+      return 1
+    }
+    if (symbol.includes("ETH")) {
+      console.log(symbol, "need to be inspected")
+    }
     return 0
   }
+  if (!whitelisted.has(symbol)) {
+    return 0
+  }
+  if (symbol === "APT" && !coin.type.startsWith("0x1::")) {
+    return 0
+  }
+
 
   const date = new Date(parseInt(timestamp) / 1000)
   const dateStr =  [date.getUTCDate(), date.getUTCMonth()+1, date.getUTCFullYear()].join("-")
@@ -61,19 +85,26 @@ export async function getPrice(symbol: string, timestamp: string) {
   const cacheKey = id + dateStr
   let price = priceCache.get(cacheKey)
   if (!price) {
-    const res = await CoinGeckoClient.coins.fetchHistory(id, {
-      date: dateStr,
-      localization: false
-    })
-    if (!res.success) {
-      console.error(res.message)
-      throw new Error(res.message)
-    }
-    if (!res.data || !res.data.market_data || !res.data.market_data.current_price) {
-      console.error("no price data for ", symbol, id, dateStr)
-      price = 0
-    } else {
-      price = res.data.market_data.current_price.usd
+    while(true) {
+      const res = await CoinGeckoClient.coins.fetchHistory(id, {
+        date: dateStr,
+        localization: false
+      })
+      if (!res.success) {
+        await delay(1000)
+        continue
+      }
+      if (!res.data || !res.data.market_data || !res.data.market_data.current_price) {
+        console.error("no price data for ", symbol, id, dateStr)
+        if (symbol === "LFC") {
+          price = 0.02864
+        } else {
+          price = 0
+        }
+      } else {
+        price = res.data.market_data.current_price.usd
+      }
+      break
     }
     priceCache.set(cacheKey, price)
   }
@@ -81,11 +112,19 @@ export async function getPrice(symbol: string, timestamp: string) {
   return price
 }
 
-export async function caculateValueInUsd(n: bigint, coinInfo: CoinInfo<any>, timestamp: string) {
-  if (coinInfo.decimals === 0) {
-    return 0
+export async function caculateValueInUsd(n: bigint, coinInfo: ExtendedCoinInfo, timestamp: string) {
+  // if (coinInfo.decimals === 0) {
+  //   return 0
+  // }
+
+  if (coinInfo.name.toLowerCase().includes("wh")) {
+    console.log(JSON.stringify(coinInfo))
   }
-  const price = await getPrice(coinInfo.symbol, timestamp)
+  const price = await getPrice(coinInfo, timestamp)
   const amount = await scaleDown(n, coinInfo.decimals)
   return amount.multipliedBy(price)
+}
+
+function delay(ms: number) {
+  return new Promise( resolve => setTimeout(resolve, ms) );
 }
