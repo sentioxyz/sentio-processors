027072e82f3e833fc444615a0ee261aaddb21a51
Fuyao Zhao <zfy0701@gmail.com>
2022-10-24 18:12:22 -0700

    wip: new way to calcuate tvl

--- projects/liquidswap/src/utils.ts
+++ projects/liquidswap/src/utils.ts
@@ -14,7 +14,35 @@ const client = new AptosClient("https://mainnet.aptoslabs.com/")
 const coinInfos = new Map<string, ExtendedCoinInfo>()
 
 // 0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa
-const whitelisted = new Set(["APT", "USDC", "USDT", "WETH" ]) // "DOGE", "UNI", "SHIBA"
+// const whitelisted = new Set(["APT", "USDC", "USDT", "WETH" ]) // "DOGE", "UNI", "SHIBA"
+
+const WHITELISTED_TOKENS: Record<string, any> = {
+  "0x1::aptos_coin::AptosCoin": { coingeckoId: "aptos", decimals: 8, },
+  "0x5e156f1207d0ebfa19a9eeff00d62a282278fb8719f4fab3a586a0a2c0fffbea::coin::T": { coingeckoId: "usd-coin", decimals: 6, }, // usdc on eth via wormhole
+  "0xa2eda21a58856fda86451436513b867c97eecb4ba099da5775520e0f7492e852::coin::T": { coingeckoId: "tether", decimals: 6, }, // via wormhole
+  "0xae478ff7d83ed072dbc5e264250e67ef58f57c99d89b447efd8a0a2e8b2be76e::coin::T": { coingeckoId: "wrapped-bitcoin", decimals: 8 }, // via wormhole
+  "0xcc8a89c8dce9693d354449f1f73e60e14e347417854f029db5bc8e7454008abb::coin::T": { coingeckoId: "ethereum", decimals: 8, }, // via wormhole
+  "0xc91d826e29a3183eb3b6f6aa3a722089fdffb8e9642b94c5fcd4c48d035c0080::coin::T": { coingeckoId: "usd-coin", decimals: 6, }, // usdc on solana via wormhole
+  "0x1000000fa32d122c18a6a31c009ce5e71674f22d06a581bb0a15575e6addadcc::usda::USDA": { coingeckoId: "usd-coin", decimals: 6, }, // USD-A
+  "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDC": { coingeckoId: "usd-coin", decimals: 6, }, // via LayerZero
+  "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDT": { coingeckoId: "tether", decimals: 6, }, // via LayerZero
+  "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::WETH": { coingeckoId: "ethereum", decimals: 6, }, // via LayerZero
+  "0xdd89c0e695df0692205912fb69fc290418bed0dbe6e4573d744a6d5e6bab6c13::coin::T": { coingeckoId: "solana", decimals: 8, },
+  "0x84d7aeef42d38a5ffc3ccef853e1b82e4958659d16a7de736a29c55fbbeb0114::staked_aptos_coin::StakedAptosCoin": { coingeckoId: "aptos", decimals: 8, },  // tortuga staked aptos
+  "0xd11107bdf0d6d7040c6c0bfbdecb6545191fdf13e8d8d259952f53e1713f61b5::staked_coin::StakedAptos": { coingeckoId: "aptos", decimals: 8, },  // ditto staked aptos
+  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::BnbCoin": { coingeckoId: "binancecoin", decimals: 8, },  // Celer
+  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::BusdCoin": { coingeckoId: "binance-usd", decimals: 8, },  // Celer
+  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::UsdcCoin": { coingeckoId: "usd-coin", decimals: 6, },  // Celer
+  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::UsdtCoin": { coingeckoId: "tether", decimals: 6, },  // Celer
+  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::DaiCoin": { coingeckoId: "dai", decimals: 8, },  // Celer
+  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::WethCoin": { coingeckoId: "ethereum", decimals: 6, },  // Celer
+  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::WbtcCoin": { coingeckoId: "wrapped-bitcoin", decimals: 8, },  // Celer
+}
+
+const WHITELISTED_SET = new Set<string>()
+for (const [k, v] of Object.entries(WHITELISTED_TOKENS)) {
+  WHITELISTED_SET.add(k)
+}
 
 interface ExtendedCoinInfo extends CoinInfo<any> {
   type: string
@@ -57,27 +85,30 @@ for (const e of csvjson) {
 }
 
 const priceCache = new Map<string, number>()
+const lastPriceCache = new Map<string, number>()
 
 export async function getPrice(coin: ExtendedCoinInfo, timestamp: string) {
+  // if (!id) {
+  //   // console.warn("price api doesn't include", symbol)
+  //   if (symbol.includes("USD")) {
+  //     return 1
+  //   }
+  //   if (symbol.includes("ETH")) {
+  //     console.log(symbol, "need to be inspected")
+  //   }
+  //   return 0
+  // }
+  if (!WHITELISTED_SET.has(coin.type)) {
+    return 0
+  }
   const symbol = coin.symbol
-  const id = symbolToId.get(symbol)
-  if (!id) {
-    // console.warn("price api doesn't include", symbol)
-    if (symbol.includes("USD")) {
-      return 1
-    }
-    if (symbol.includes("ETH")) {
-      console.log(symbol, "need to be inspected")
-    }
-    return 0
-  }
-  if (!whitelisted.has(symbol)) {
-    return 0
-  }
-  if (symbol === "APT" && !coin.type.startsWith("0x1::")) {
-    return 0
-  }
 
+  // if (symbol === "APT" && !coin.type.startsWith("0x1::")) {
+  //   return 0
+  // }
+
+  // const x =
+  const id = WHITELISTED_TOKENS[coin.type].coingeckoId
 
   const date = new Date(parseInt(timestamp) / 1000)
   const dateStr =  [date.getUTCDate(), date.getUTCMonth()+1, date.getUTCFullYear()].join("-")
@@ -96,17 +127,23 @@ export async function getPrice(coin: ExtendedCoinInfo, timestamp: string) {
       }
       if (!res.data || !res.data.market_data || !res.data.market_data.current_price) {
         console.error("no price data for ", symbol, id, dateStr)
-        if (symbol === "LFC") {
-          price = 0.02864
-        } else {
-          price = 0
-        }
+        // if (symbol === "LFC") {
+        //   price = 0.02864
+        // } else {
+          // const lastPrice =
+          // if (lastPrice) {
+          //   return
+          // }
+        price = lastPriceCache.get(id) || 0
+
+        // }
       } else {
         price = res.data.market_data.current_price.usd
       }
       break
     }
     priceCache.set(cacheKey, price)
+    lastPriceCache.set(id, price)
   }
 
   return price
