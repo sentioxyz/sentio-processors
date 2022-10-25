9f308112a1fcceaf45e02a74e89bcdaeefde1f7b
Fuyao Zhao <zfy0701@gmail.com>
2022-10-24 18:12:22 -0700

    new way to calcuate tvl

--- projects/liquidswap/src/processor.ts
+++ projects/liquidswap/src/processor.ts
@@ -1,77 +1,118 @@
 import { liquidity_pool } from './types/aptos/liquidswap'
-import { aptos, Counter, Gauge } from "@sentio/sdk";
-import { caculateValueInUsd, getCoinInfo, scaleDown } from "./utils";
+import { AccountEventTracker, aptos, Counter, Gauge } from "@sentio/sdk";
+import { caculateValueInUsd, delay, getCoinInfo, scaleDown } from "./utils";
 import { coin } from "@sentio/sdk/lib/builtin/aptos/0x1";
+import { getRpcClient } from "@sentio/sdk/lib/aptos";
+import { AptosClient } from "aptos-sdk";
 
-const valueCounter = new Counter("value")
-const amountCounter = new Counter("amount")
-const volumeGauge = new Gauge("vol")
+import * as crypto from "crypto"
+
+const commonOptions = { sparse:  true }
+const valueCounter = new Gauge("value", commonOptions)
+const amountCounter = new Gauge("amount", commonOptions)
+const volumeGauge = new Gauge("vol", commonOptions)
+const eventCounter = new Counter("num_event", commonOptions)
+
+// const accountTracker = AccountEventTracker.register("trading")
 
 liquidity_pool.bind({startVersion: 0})
   .onEventPoolCreatedEvent(async (evt, ctx) => {
-    const pool = await getPairNames(evt.type_arguments)
-    ctx.meter.Counter("number_pool").add(1, { pool: pool })
+    const pool = await getPoolName(evt.type_arguments)
+    ctx.meter.Counter("num_pools").add(1, { pool })
+    // eventCounter.add(ctx, 1, { name: "pool_created"})
+    await syncPools(ctx)
   })
   .onEventLiquidityAddedEvent(async (evt, ctx) => {
-    const pool = await getPairNames(evt.type_arguments)
-    const timestamp = ctx.transaction.timestamp
-
-    await addFor(ctx, evt.type_arguments[0], evt.data_typed.added_x_val, timestamp, pool)
-    await addFor(ctx, evt.type_arguments[1], evt.data_typed.added_y_val, timestamp, pool)
+    // const pool = await getPoolName(evt.type_arguments)
+    // const timestamp = ctx.transaction.timestamp
+    ctx.meter.Counter("event_liquidity_add").add(1)
+    await syncPools(ctx)
   })
   .onEventLiquidityRemovedEvent(async (evt, ctx) => {
-    const  pool = await getPairNames(evt.type_arguments)
-    const timestamp = ctx.transaction.timestamp
-
-    await subFor(ctx, evt.type_arguments[0], evt.data_typed.returned_x_val, timestamp, pool)
-    await subFor(ctx, evt.type_arguments[1], evt.data_typed.returned_y_val, timestamp, pool)
+    // const  pool = await getPoolName(evt.type_arguments)
+    // const timestamp = ctx.transaction.timestamp
+    ctx.meter.Counter("event_liquidity_removed").add(1)
+    await syncPools(ctx)
   })
   .onEventSwapEvent(async (evt, ctx) => {
-    const pool = await getPairNames(evt.type_arguments)
+    const pool = await getPoolName(evt.type_arguments)
     const timestamp = ctx.transaction.timestamp
 
-    await addFor(ctx, evt.type_arguments[0], evt.data_typed.x_in, timestamp, pool, true)
-    await addFor(ctx, evt.type_arguments[1], evt.data_typed.y_in, timestamp, pool, true)
-    await subFor(ctx, evt.type_arguments[0], evt.data_typed.x_out, timestamp, pool)
-    await subFor(ctx, evt.type_arguments[1], evt.data_typed.y_out, timestamp, pool)
-    ctx.meter.Counter("number_swap").add(1)
+    // accountTracker.trackEvent(ctx, { distinctId: evt.guid.account_address })
+    await addForVolume(ctx, evt.type_arguments[0], evt.data_typed.x_in, timestamp, pool)
+    await addForVolume(ctx, evt.type_arguments[1], evt.data_typed.y_in, timestamp, pool)
+    ctx.meter.Counter("event_swap").add(1)
+    await syncPools(ctx)
   })
   .onEventFlashloanEvent(async (evt, ctx) => {
-    const pool = await getPairNames(evt.type_arguments)
+    const pool = await getPoolName(evt.type_arguments)
     const timestamp = ctx.transaction.timestamp
 
-    await addFor(ctx, evt.type_arguments[0], evt.data_typed.x_in, timestamp, pool, true)
-    await addFor(ctx, evt.type_arguments[1], evt.data_typed.y_in, timestamp, pool, true)
-    await subFor(ctx, evt.type_arguments[0], evt.data_typed.x_out, timestamp, pool)
-    await subFor(ctx, evt.type_arguments[1], evt.data_typed.y_out, timestamp, pool)
+    // accountTracker.trackEvent(ctx, { distinctId: evt.guid.account_address })
+    await addForVolume(ctx, evt.type_arguments[0], evt.data_typed.x_in, timestamp, pool)
+    await addForVolume(ctx, evt.type_arguments[1], evt.data_typed.y_in, timestamp, pool)
 
-    ctx.meter.Counter("number_flashloan").add(1)
+    ctx.meter.Counter("event_flashloan").add(1)
+    await syncPools(ctx)
   })
 
-async function addFor(ctx: aptos.AptosContext, type: string, amount: bigint, timestamp: string, pool: string, recordTrading = false) {
+async function addForVolume(ctx: aptos.AptosContext, type: string, amount: bigint, timestamp: string, pool: string) {
   const coin = await getCoinInfo(type)
   const value = await caculateValueInUsd(amount, coin, timestamp)
-  valueCounter.add(ctx, value, { coin: coin.symbol, pool: pool })
-  amountCounter.add(ctx, scaleDown(amount, coin.decimals) , { coin: coin.symbol, pool: pool })
-  if (recordTrading) {
-    volumeGauge.record(ctx, value, { coin: coin.symbol, pool: pool })
-  }
+  volumeGauge.record(ctx, value, { coin: coin.symbol, pool: pool })
 }
 
-async function subFor(ctx: aptos.AptosContext, type: string, amount: bigint, timestamp: string, pool: string) {
+async function addFor(ctx: aptos.AptosContext, type: string, amount: bigint, timestamp: string, pool: string) {
   const coin = await getCoinInfo(type)
   const value = await caculateValueInUsd(amount, coin, timestamp)
-  valueCounter.sub(ctx, value, { coin: coin.symbol, pool: pool })
-  amountCounter.sub(ctx, scaleDown(amount, coin.decimals), { coin: coin.symbol, pool: pool })
+  valueCounter.record(ctx, value, { coin: coin.symbol, pool: pool })
+  amountCounter.record(ctx, scaleDown(amount, coin.decimals) , { coin: coin.symbol, pool: pool })
 }
 
-async function getPairNames(coins: [string, string, string]): Promise<string> {
+// async function subFor(ctx: aptos.AptosContext, type: string, amount: bigint, timestamp: string, pool: string) {
+//   const coin = await getCoinInfo(type)
+//   const value = await caculateValueInUsd(amount, coin, timestamp)
+//   valueCounter.sub(ctx, value, { coin: coin.symbol, pool: pool })
+//   amountCounter.sub(ctx, scaleDown(amount, coin.decimals), { coin: coin.symbol, pool: pool })
+// }
+
+// TODO pool name should consider not just use symbol name
+async function getPoolName(coins: [string, string, string]): Promise<string> {
   const coinx = await getCoinInfo(coins[0])
   const coiny = await getCoinInfo(coins[1])
   const token = coins[2].includes("curves::Stable") ? "S" : "U"
   // const xfullname = coins[0].split("::").slice(1).join("::")
   // const yfullname = coins[1].split("::").slice(1).join("::")
-  const pool = `${coinx.symbol}-${coiny.symbol}-${token}`
-  return pool
+  const id = crypto.createHash("md5").update(coins.join()).digest("hex")
+  return `${coinx.symbol}-${coiny.symbol}-${token}-${id.slice(0, 6)}`
    // return [coinx, coiny, pool]
+}
+
+const recorded = new Set<bigint>()
+
+async function syncPools(ctx: aptos.AptosContext) {
+  const version = BigInt(ctx.version.toString())
+  const bucket = version / 100000n;
+  if (recorded.has(bucket)) {
+    return
+  }
+  recorded.add(bucket)
+
+  // const client = getRpcClient(aptos.AptosNetwork.MAIN_NET)
+  const client = new AptosClient("https://mainnet.aptoslabs.com/")
+  // const client = new AptosClient("https://aptos-mainnet.nodereal.io/v1/f0bd2babe59b4308b49df438b7c4ef45")
+
+  let resources = undefined
+  while (!resources) {
+    resources = await client.getAccountResources('0x5a97986a9d031c4567e15b797be516910cfcb4156312482efc6a19c0a30c948', { ledgerVersion: version})
+    await delay(1000)
+  }
+
+  const pools = aptos.TYPE_REGISTRY.filterAndDecodeResources<liquidity_pool.LiquidityPool<any, any, any>>("0x190d44266241744264b964a37b8f09863167a12d3e70cda39376cfb4e3561e12::liquidity_pool::LiquidityPool", resources)
+
+  for (const pool of pools) {
+    const poolName = await getPoolName(pool.type_arguments as [string,string,string])
+    await addFor(ctx, pool.type_arguments[0], pool.data_typed.coin_x_reserve.value, ctx.transaction.timestamp, poolName)
+    await addFor(ctx, pool.type_arguments[1], pool.data_typed.coin_y_reserve.value, ctx.transaction.timestamp, poolName)
+  }
 }
