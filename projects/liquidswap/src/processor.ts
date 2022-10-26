fe148b6a58d685481b8b6a690b331ac13595c9c3
Fuyao Zhao <zfy0701@gmail.com>
2022-10-26 16:00:14 -0700

    intermidate results

--- projects/liquidswap/src/processor.ts
+++ projects/liquidswap/src/processor.ts
@@ -1,73 +1,120 @@
 import { liquidity_pool } from './types/aptos/liquidswap'
 import { AccountEventTracker, aptos, Counter, Gauge } from "@sentio/sdk";
-import { caculateValueInUsd, delay, getCoinInfo, scaleDown } from "./utils";
+import { caculateValueInUsd, delay, getCoinInfo, scaleDown, whiteListed } from "./utils";
 import { coin } from "@sentio/sdk/lib/builtin/aptos/0x1";
 import { getRpcClient } from "@sentio/sdk/lib/aptos";
 import { AptosClient } from "aptos-sdk";
 
 import * as crypto from "crypto"
+import { BigDecimal } from "@sentio/sdk/lib/core/big-decimal";
 
 const commonOptions = { sparse:  true }
-const valueCounter = new Gauge("value", commonOptions)
+const valueByPool = new Gauge("value_by_pool", commonOptions)
+const valueByCoin = new Gauge("value_by_coin", commonOptions)
 const amountCounter = new Gauge("amount", commonOptions)
 const volumeGauge = new Gauge("vol", commonOptions)
-const eventCounter = new Counter("num_event", commonOptions)
+const volumeByBridge = new Gauge("vol_by_bridge", commonOptions)
+
+// const eventCounter = new Counter("num_event", commonOptions)
 
 // const accountTracker = AccountEventTracker.register("trading")
 
-liquidity_pool.bind({startVersion: 0})
+liquidity_pool.bind({startVersion: 29999})
   .onEventPoolCreatedEvent(async (evt, ctx) => {
-    const pool = await getPoolName(evt.type_arguments)
-    ctx.meter.Counter("num_pools").add(1, { pool })
-    // eventCounter.add(ctx, 1, { name: "pool_created"})
+    ctx.meter.Counter("num_pools").add(1)
     await syncPools(ctx)
   })
   .onEventLiquidityAddedEvent(async (evt, ctx) => {
-    // const pool = await getPoolName(evt.type_arguments)
-    // const timestamp = ctx.transaction.timestamp
     ctx.meter.Counter("event_liquidity_add").add(1)
     await syncPools(ctx)
   })
   .onEventLiquidityRemovedEvent(async (evt, ctx) => {
-    // const  pool = await getPoolName(evt.type_arguments)
-    // const timestamp = ctx.transaction.timestamp
     ctx.meter.Counter("event_liquidity_removed").add(1)
     await syncPools(ctx)
   })
   .onEventSwapEvent(async (evt, ctx) => {
     const pool = await getPoolName(evt.type_arguments)
-    const timestamp = ctx.transaction.timestamp
+    await recordTradingVolume(ctx, evt.type_arguments[0], evt.type_arguments[1], evt.data_typed.x_in, evt.data_typed.y_in, pool)
 
-    // accountTracker.trackEvent(ctx, { distinctId: evt.guid.account_address })
-    await addForVolume(ctx, evt.type_arguments[0], evt.data_typed.x_in, timestamp, pool)
-    await addForVolume(ctx, evt.type_arguments[1], evt.data_typed.y_in, timestamp, pool)
-    ctx.meter.Counter("event_swap").add(1)
+    const coinXInfo = await getCoinInfo(evt.type_arguments[0])
+    const coinYInfo = await getCoinInfo(evt.type_arguments[1])
+    ctx.meter.Counter("event_swap_total").add(1)
+    ctx.meter.Counter("event_swap_by_bridge").add(1, { bridge: coinXInfo.bridge })
+    ctx.meter.Counter("event_swap_by_bridge").add(1, { bridge: coinYInfo.bridge })
+
     await syncPools(ctx)
   })
   .onEventFlashloanEvent(async (evt, ctx) => {
     const pool = await getPoolName(evt.type_arguments)
-    const timestamp = ctx.transaction.timestamp
+    await recordTradingVolume(ctx, evt.type_arguments[0], evt.type_arguments[1], evt.data_typed.x_in, evt.data_typed.y_in, pool)
 
-    // accountTracker.trackEvent(ctx, { distinctId: evt.guid.account_address })
-    await addForVolume(ctx, evt.type_arguments[0], evt.data_typed.x_in, timestamp, pool)
-    await addForVolume(ctx, evt.type_arguments[1], evt.data_typed.y_in, timestamp, pool)
-
+    const coinXInfo = await getCoinInfo(evt.type_arguments[0])
+    const coinYInfo = await getCoinInfo(evt.type_arguments[1])
     ctx.meter.Counter("event_flashloan").add(1)
+    ctx.meter.Counter("event_flashloan_by_bridge").add(1, { bridge: coinXInfo.bridge })
+    ctx.meter.Counter("event_flashloan_by_bridge").add(1, { bridge: coinYInfo.bridge })
+
     await syncPools(ctx)
   })
 
-async function addForVolume(ctx: aptos.AptosContext, type: string, amount: bigint, timestamp: string, pool: string) {
-  const coin = await getCoinInfo(type)
-  const value = await caculateValueInUsd(amount, coin, timestamp)
-  volumeGauge.record(ctx, value, { coin: coin.symbol, pool: pool })
+// async function addForVolume(ctx: aptos.AptosContext, type: string, amount: bigint, timestamp: string, pool: string, valueOverride?: BigDecimal): Promise<BigDecimal> {
+//   if (!whiteListed(type)) {
+//     if (valueOverride) {
+//       const coin = await getCoinInfo(type)
+//       volumeGauge.record(ctx, value, {coin: coin.symbol, pool: pool})
+//       return valueOverride
+//     }
+//   }
+//   const coin = await getCoinInfo(type)
+//   const value = await caculateValueInUsd(amount, coin, timestamp)
+//   volumeGauge.record(ctx, value, {coin: coin.symbol, pool: pool})
+//   return value
+// }
+
+async function recordTradingVolume(ctx: aptos.AptosContext, coinx: string, coiny: string, coinx_amount: bigint, coiny_amount: bigint, poolName: string) {
+  // const coinx = pool.type_arguments[0]
+  // const coiny = pool.type_arguments[1]
+
+  const whitelistx = whiteListed(coinx)
+  const whitelisty = whiteListed(coiny)
+  if (!whitelistx && !whitelisty) {
+    return
+  }
+
+  const timestamp = ctx.transaction.timestamp
+  const coinXInfo = await getCoinInfo(coinx)
+  const coinYInfo = await getCoinInfo(coiny)
+
+  if (whitelistx) {
+    const value = await caculateValueInUsd(coinx_amount, coinXInfo, timestamp)
+    volumeGauge.record(ctx, value, {coin: coinXInfo.symbol, pool: poolName})
+    volumeByBridge.record(ctx, value, {coin: coinXInfo.symbol, bridge: coinXInfo.bridge})
+    // if (coinXInfo.bridge)
+
+    if (!whitelisty) {
+      volumeGauge.record(ctx, value, {coin: coinYInfo.symbol, pool: poolName})
+    }
+  }
+  if (whitelisty) {
+    const value = await caculateValueInUsd(coiny_amount, coinYInfo, timestamp)
+    volumeGauge.record(ctx, value, {coin: coinYInfo.symbol, pool: poolName})
+    volumeByBridge.record(ctx, value, {coin: coinYInfo.symbol, bridge: coinXInfo.bridge})
+
+    if (!whitelistx) {
+      volumeGauge.record(ctx, value, {coin: coinXInfo.symbol, pool: poolName})
+    }
+  }
+
 }
 
-async function addFor(ctx: aptos.AptosContext, type: string, amount: bigint, timestamp: string, pool: string) {
-  const coin = await getCoinInfo(type)
-  const value = await caculateValueInUsd(amount, coin, timestamp)
-  valueCounter.record(ctx, value, { coin: coin.symbol, pool: pool })
-  amountCounter.record(ctx, scaleDown(amount, coin.decimals) , { coin: coin.symbol, pool: pool })
-}
+// async function addFor(ctx: aptos.AptosContext, type: string, amount: bigint, timestamp: string, pool: string) {
+//   const coin = await getCoinInfo(type)
+//   if (coin) {
+//     const value = await caculateValueInUsd(amount, coin, timestamp)
+//     valueCounter.record(ctx, value, {coin: coin.symbol, pool: pool})
+//     amountCounter.record(ctx, scaleDown(amount, coin.decimals), {coin: coin.symbol, pool: pool})
+//   }
+// }
 
 // async function subFor(ctx: aptos.AptosContext, type: string, amount: bigint, timestamp: string, pool: string) {
 //   const coin = await getCoinInfo(type)
@@ -80,6 +127,9 @@ async function addFor(ctx: aptos.AptosContext, type: string, amount: bigint, tim
 async function getPoolName(coins: [string, string, string]): Promise<string> {
   const coinx = await getCoinInfo(coins[0])
   const coiny = await getCoinInfo(coins[1])
+  // if (!coinx || !coiny) {
+  //   return undefined
+  // }
   const token = coins[2].includes("curves::Stable") ? "S" : "U"
   // const xfullname = coins[0].split("::").slice(1).join("::")
   // const yfullname = coins[1].split("::").slice(1).join("::")
@@ -99,20 +149,79 @@ async function syncPools(ctx: aptos.AptosContext) {
   recorded.add(bucket)
 
   // const client = getRpcClient(aptos.AptosNetwork.MAIN_NET)
-  const client = new AptosClient("https://mainnet.aptoslabs.com/")
-  // const client = new AptosClient("https://aptos-mainnet.nodereal.io/v1/f0bd2babe59b4308b49df438b7c4ef45")
-
+  // const client = new AptosClient("https://mainnet.aptoslabs.com/")
+  const client = new AptosClient("https://aptos-mainnet.nodereal.io/v1/0c58c879d41e4eab8fd2fc0406848c2b")
+  //
   let resources = undefined
   while (!resources) {
-    resources = await client.getAccountResources('0x5a97986a9d031c4567e15b797be516910cfcb4156312482efc6a19c0a30c948', { ledgerVersion: version})
-    await delay(1000)
+    try {
+      resources = await client.getAccountResources('0x5a97986a9d031c4567e15b797be516910cfcb4156312482efc6a19c0a30c948', {ledgerVersion: version})
+    } catch (e) {
+      console.log("rpc error, retrying", e)
+      await delay(1000)
+    }
   }
 
   const pools = aptos.TYPE_REGISTRY.filterAndDecodeResources<liquidity_pool.LiquidityPool<any, any, any>>("0x190d44266241744264b964a37b8f09863167a12d3e70cda39376cfb4e3561e12::liquidity_pool::LiquidityPool", resources)
+  const volumeByCoin = new Map<string, BigDecimal>()
 
+  console.log("num of pools: ", pools.length)
   for (const pool of pools) {
+    const coinx = pool.type_arguments[0]
+    const coiny = pool.type_arguments[1]
+    const whitelistx = whiteListed(coinx)
+    const whitelisty = whiteListed(coiny)
+    if (!whitelistx && !whitelisty) {
+      continue
+    }
+
+    const coinXInfo = await getCoinInfo(coinx)
+    const coinYInfo = await getCoinInfo(coiny)
+    const timestamp = ctx.transaction.timestamp
+
+    const coinx_amount = pool.data_typed.coin_x_reserve.value
+    const coiny_amount = pool.data_typed.coin_y_reserve.value
+
     const poolName = await getPoolName(pool.type_arguments as [string,string,string])
-    await addFor(ctx, pool.type_arguments[0], pool.data_typed.coin_x_reserve.value, ctx.transaction.timestamp, poolName)
-    await addFor(ctx, pool.type_arguments[1], pool.data_typed.coin_y_reserve.value, ctx.transaction.timestamp, poolName)
+
+    amountCounter.record(ctx, scaleDown(coinx_amount, coinXInfo.decimals), {coin: coinXInfo.symbol, pool: poolName})
+    amountCounter.record(ctx, scaleDown(coiny_amount, coinYInfo.decimals), {coin: coinYInfo.symbol, pool: poolName})
+
+    if (whitelistx) {
+      const value = await caculateValueInUsd(coinx_amount, coinXInfo, timestamp)
+      valueByPool.record(ctx, value, {coin: coinXInfo.symbol, pool: poolName})
+
+      let coinXTotal = volumeByCoin.get(coinXInfo.type)
+      if (!coinXTotal) {
+        coinXTotal = value
+      }
+      coinXTotal = coinXTotal.plus(value)
+      volumeByCoin.set(coinXInfo.type, coinXTotal)
+
+      if (!whitelisty) {
+        valueByPool.record(ctx, value, {coin: coinYInfo.symbol, pool: poolName})
+      }
+    }
+    if (whitelisty) {
+      const value = await caculateValueInUsd(coiny_amount, coinYInfo, timestamp)
+      valueByPool.record(ctx, value, {coin: coinYInfo.symbol, pool: poolName})
+
+      let coinYTotal = volumeByCoin.get(coinYInfo.type)
+      if (!coinYTotal) {
+        coinYTotal = value
+      }
+      coinYTotal = coinYTotal.plus(value)
+      volumeByCoin.set(coinYInfo.type, coinYTotal)
+
+      if (!whitelistx) {
+        valueByPool.record(ctx, value, {coin: coinXInfo.symbol, pool: poolName})
+      }
+    }
+  }
+
+  for (const [k, v] of volumeByCoin) {
+    const coinInfo = await getCoinInfo(k)
+
+    valueByCoin.record(ctx, v, { coin: coinInfo.symbol, bridge: coinInfo.bridge })
   }
 }
--- projects/liquidswap/src/utils.ts
+++ projects/liquidswap/src/utils.ts
@@ -9,7 +9,7 @@ import Coin = coin.Coin;
 
 const CoinGeckoClient = new CoinGecko();
 
-const client = new AptosClient("https://mainnet.aptoslabs.com/")
+const client = new AptosClient("https://aptos-mainnet.nodereal.io/v1/0c58c879d41e4eab8fd2fc0406848c2b/")
 
 const coinInfos = new Map<string, ExtendedCoinInfo>()
 
@@ -17,26 +17,26 @@ const coinInfos = new Map<string, ExtendedCoinInfo>()
 // const whitelisted = new Set(["APT", "USDC", "USDT", "WETH" ]) // "DOGE", "UNI", "SHIBA"
 
 const WHITELISTED_TOKENS: Record<string, any> = {
-  "0x1::aptos_coin::AptosCoin": { coingeckoId: "aptos", decimals: 8, },
-  "0x5e156f1207d0ebfa19a9eeff00d62a282278fb8719f4fab3a586a0a2c0fffbea::coin::T": { coingeckoId: "usd-coin", decimals: 6, }, // usdc on eth via wormhole
-  "0xa2eda21a58856fda86451436513b867c97eecb4ba099da5775520e0f7492e852::coin::T": { coingeckoId: "tether", decimals: 6, }, // via wormhole
-  "0xae478ff7d83ed072dbc5e264250e67ef58f57c99d89b447efd8a0a2e8b2be76e::coin::T": { coingeckoId: "wrapped-bitcoin", decimals: 8 }, // via wormhole
-  "0xcc8a89c8dce9693d354449f1f73e60e14e347417854f029db5bc8e7454008abb::coin::T": { coingeckoId: "ethereum", decimals: 8, }, // via wormhole
-  "0xc91d826e29a3183eb3b6f6aa3a722089fdffb8e9642b94c5fcd4c48d035c0080::coin::T": { coingeckoId: "usd-coin", decimals: 6, }, // usdc on solana via wormhole
-  "0x1000000fa32d122c18a6a31c009ce5e71674f22d06a581bb0a15575e6addadcc::usda::USDA": { coingeckoId: "usd-coin", decimals: 6, }, // USD-A
-  "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDC": { coingeckoId: "usd-coin", decimals: 6, }, // via LayerZero
-  "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDT": { coingeckoId: "tether", decimals: 6, }, // via LayerZero
-  "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::WETH": { coingeckoId: "ethereum", decimals: 6, }, // via LayerZero
-  "0xdd89c0e695df0692205912fb69fc290418bed0dbe6e4573d744a6d5e6bab6c13::coin::T": { coingeckoId: "solana", decimals: 8, },
-  "0x84d7aeef42d38a5ffc3ccef853e1b82e4958659d16a7de736a29c55fbbeb0114::staked_aptos_coin::StakedAptosCoin": { coingeckoId: "aptos", decimals: 8, },  // tortuga staked aptos
-  "0xd11107bdf0d6d7040c6c0bfbdecb6545191fdf13e8d8d259952f53e1713f61b5::staked_coin::StakedAptos": { coingeckoId: "aptos", decimals: 8, },  // ditto staked aptos
-  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::BnbCoin": { coingeckoId: "binancecoin", decimals: 8, },  // Celer
-  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::BusdCoin": { coingeckoId: "binance-usd", decimals: 8, },  // Celer
-  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::UsdcCoin": { coingeckoId: "usd-coin", decimals: 6, },  // Celer
-  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::UsdtCoin": { coingeckoId: "tether", decimals: 6, },  // Celer
-  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::DaiCoin": { coingeckoId: "dai", decimals: 8, },  // Celer
-  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::WethCoin": { coingeckoId: "ethereum", decimals: 6, },  // Celer
-  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::WbtcCoin": { coingeckoId: "wrapped-bitcoin", decimals: 8, },  // Celer
+  "0x1::aptos_coin::AptosCoin": { coingeckoId: "aptos", decimals: 8, bridge: "none" },
+  "0x5e156f1207d0ebfa19a9eeff00d62a282278fb8719f4fab3a586a0a2c0fffbea::coin::T": { coingeckoId: "usd-coin", decimals: 6, bridge: "wormhole" }, // usdc on eth via wormhole
+  "0xa2eda21a58856fda86451436513b867c97eecb4ba099da5775520e0f7492e852::coin::T": { coingeckoId: "tether", decimals: 6,  bridge: "wormhole" }, // via wormhole
+  "0xae478ff7d83ed072dbc5e264250e67ef58f57c99d89b447efd8a0a2e8b2be76e::coin::T": { coingeckoId: "wrapped-bitcoin", decimals: 8, bridge: "wormhole"  }, // via wormhole
+  "0xcc8a89c8dce9693d354449f1f73e60e14e347417854f029db5bc8e7454008abb::coin::T": { coingeckoId: "ethereum", decimals: 8,  bridge: "wormhole" }, // via wormhole
+  "0xc91d826e29a3183eb3b6f6aa3a722089fdffb8e9642b94c5fcd4c48d035c0080::coin::T": { coingeckoId: "usd-coin", decimals: 6,  bridge: "wormhole" }, // usdc on solana via wormhole
+  "0x1000000fa32d122c18a6a31c009ce5e71674f22d06a581bb0a15575e6addadcc::usda::USDA": { coingeckoId: "usd-coin", decimals: 6,  bridge: "none" }, // USD-A
+  "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDC": { coingeckoId: "usd-coin", decimals: 6, bridge: "LayerZero"}, // via LayerZero
+  "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDT": { coingeckoId: "tether", decimals: 6,  bridge: "LayerZero"}, // via LayerZero
+  "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::WETH": { coingeckoId: "ethereum", decimals: 6,  bridge: "LayerZero"}, // via LayerZero
+  "0xdd89c0e695df0692205912fb69fc290418bed0dbe6e4573d744a6d5e6bab6c13::coin::T": { coingeckoId: "solana", decimals: 8,  bridge: "wormhole" },
+  "0x84d7aeef42d38a5ffc3ccef853e1b82e4958659d16a7de736a29c55fbbeb0114::staked_aptos_coin::StakedAptosCoin": { coingeckoId: "aptos", decimals: 8, bridge: "none" },  // tortuga staked aptos
+  "0xd11107bdf0d6d7040c6c0bfbdecb6545191fdf13e8d8d259952f53e1713f61b5::staked_coin::StakedAptos": { coingeckoId: "aptos", decimals: 8, bridge: "none" },  // ditto staked aptos
+  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::BnbCoin": { coingeckoId: "binancecoin", decimals: 8, bridge: "Celer" },  // Celer
+  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::BusdCoin": { coingeckoId: "binance-usd", decimals: 8, bridge: "Celer" },  // Celer
+  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::UsdcCoin": { coingeckoId: "usd-coin", decimals: 6, bridge: "Celer"},  // Celer
+  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::UsdtCoin": { coingeckoId: "tether", decimals: 6,bridge: "Celer" },  // Celer
+  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::DaiCoin": { coingeckoId: "dai", decimals: 8, bridge: "Celer"},  // Celer
+  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::WethCoin": { coingeckoId: "ethereum", decimals: 6,bridge: "Celer" },  // Celer
+  "0x8d87a65ba30e09357fa2edea2c80dbac296e5dec2b18287113500b902942929d::celer_coin_manager::WbtcCoin": { coingeckoId: "wrapped-bitcoin", decimals: 8,bridge: "Celer" },  // Celer
 }
 
 const WHITELISTED_SET = new Set<string>()
@@ -44,30 +44,41 @@ for (const [k, v] of Object.entries(WHITELISTED_TOKENS)) {
   WHITELISTED_SET.add(k)
 }
 
-interface ExtendedCoinInfo extends CoinInfo<any> {
+export function whiteListed(type: string): boolean {
+  return WHITELISTED_SET.has(type)
+}
+
+interface ExtendedCoinInfo {
   type: string
+  symbol: string
+  decimals: number
+  bridge: string
 }
 
 export async function getCoinInfo(type: string): Promise<ExtendedCoinInfo> {
+  if (!whiteListed(type)){
+    return {
+      type,
+      symbol: type.split("::")[2],
+      decimals: 1,
+      bridge: "none"
+    }
+  }
   let extInfo = coinInfos.get(type)
   if (!extInfo) {
     const parts = type.split("::")
     const account = parts[0]
-    try {
-      const resource = await client.getAccountResource(account, `0x1::coin::CoinInfo<${type}>`)
-      extInfo = { ...resource.data as CoinInfo<any>, type }
-    } catch (e) {
-      extInfo = {
-        name: parts[2],
-        symbol: parts[2],
-        decimals: 0,
-        supply: {vec:""},
-        type
+    while (!extInfo)
+      try {
+        const resource = await client.getAccountResource(account, `0x1::coin::CoinInfo<${type}>`)
+        extInfo = {...resource.data as CoinInfo<any>, type, bridge: WHITELISTED_TOKENS[type].bridge }
+      } catch (e) {
+        console.log("rpc error get coin info", type, e)
+        await delay(1000)
       }
-    }
-    coinInfos.set(type, extInfo)
   }
-  return extInfo!
+  coinInfos.set(type, extInfo)
+  return extInfo
 }
 
 export function scaleDown(n: bigint, decimal: number) {
@@ -98,10 +109,13 @@ export async function getPrice(coin: ExtendedCoinInfo, timestamp: string) {
   //   }
   //   return 0
   // }
-  if (!WHITELISTED_SET.has(coin.type)) {
+  if (!whiteListed(coin.type)) {
     return 0
   }
   const symbol = coin.symbol
+  if (symbol === 'T') {
+    console.error(symbol, JSON.stringify(coin), timestamp)
+  }
 
   // if (symbol === "APT" && !coin.type.startsWith("0x1::")) {
   //   return 0
@@ -153,10 +167,10 @@ export async function caculateValueInUsd(n: bigint, coinInfo: ExtendedCoinInfo, 
   // if (coinInfo.decimals === 0) {
   //   return 0
   // }
-
-  if (coinInfo.name.toLowerCase().includes("wh")) {
-    console.log(JSON.stringify(coinInfo))
-  }
+  //
+  // if (coinInfo.name.toLowerCase().includes("wh")) {
+  //   console.log(JSON.stringify(coinInfo))
+  // }
   const price = await getPrice(coinInfo, timestamp)
   const amount = await scaleDown(n, coinInfo.decimals)
   return amount.multipliedBy(price)
