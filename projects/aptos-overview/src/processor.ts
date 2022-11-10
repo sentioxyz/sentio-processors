14597f315bbeed869f3089ad8da73d38b25e5490
Fuyao Zhao <zfy0701@gmail.com>
2022-11-10 14:58:10 -0800

    add aptos overview

---
+++ projects/aptos-overview/src/processor.ts
@@ -0,0 +1,82 @@
+import { AccountEventTracker, aptos, Counter, Gauge } from "@sentio/sdk";
+import { aptos_coin, coin, managed_coin, resource_account, aptos_account } from "@sentio/sdk/lib/builtin/aptos/0x1";
+
+import { DEFAULT_MAINNET_LIST, RawCoinInfo } from "@manahippo/coin-list/dist/list";
+import { liquidity_pool, scripts } from "./types/aptos/liquidswap";
+import { amm } from "./types/aptos/auxexchange";
+import { TransactionPayload_EntryFunctionPayload } from "aptos-sdk/src/generated";
+import { router, swap } from "./types/aptos/pancake-swap";
+import { token, token_transfers } from "@sentio/sdk/lib/builtin/aptos/0x3";
+import { getChainQueryClient } from "@sentio/sdk/lib/aptos/utils";
+
+const txnCounter = new Counter("txn_counter")
+
+const coinInfoMap = new Map<string, RawCoinInfo>()
+
+for (const x of DEFAULT_MAINNET_LIST) {
+  coinInfoMap.set(x.token_type.type, x)
+}
+
+// resource_account.bind()
+//   .onEntryCreateResourceAccount((call, ctx) => {
+//     txnCounter.add(ctx, 1, { kind: "account_creation_2"})
+//   })
+//   .onEntryCreateResourceAccountAndFund((call, ctx) => {
+//     txnCounter.add(ctx, 1, { kind: "account_creation"})
+//   })
+//   .onEntryCreateResourceAccountAndPublishPackage((call, ctx) => {
+//     txnCounter.add(ctx, 1, { kind: "account_creation"})
+//   })
+
+const queryClient = getChainQueryClient()
+// await queryClient.aptosSQLQuery("asdfsf")
+
+managed_coin.bind()
+  .onEntryRegister((call, ctx) => {
+    txnCounter.add(ctx, 1, { kind: "register"})
+  })
+
+coin.bind()
+  .onEntryTransfer((call, ctx) => {
+    const info = coinInfoMap.get(call.type_arguments[0])
+    let symbol = info ? info.symbol : "others"
+    txnCounter.add(ctx, 1, { kind: "transfer", symbol})
+  })
+
+aptos_account.bind()
+    .onEntryCreateAccount((call, ctx) => {
+      txnCounter.add(ctx, 1, { kind: "account_creation"})
+    })
+    .onEntryTransfer((call, ctx) => {
+      txnCounter.add(ctx, 1, { kind: "transfer", symbol: "native" })
+    })
+//
+// token_transfers.bind()
+//     .onTransaction((txn, ctx) => {
+//       txnCounter.add(ctx, 1, { kind: "transfer", symbol: 'nft' })
+//     })
+
+// token.bind()
+//   .onTransaction((txn, ctx) => {
+//     txnCounter.add(ctx, 1, { kind: "nft"})
+//   })
+
+// swaps
+scripts.bind()
+  .onTransaction((tx, ctx) => {
+    txnCounter.add(ctx, 1, { kind: "swap", protocol: "liquidswap"})
+  })
+
+amm.bind()
+  .onTransaction((tx, ctx) => {
+    txnCounter.add(ctx, 1, { kind: "swap", protocol: "aux"})
+  })
+
+router.bind()
+  .onTransaction((tx, ctx) => {
+    txnCounter.add(ctx, 1, { kind: "swap", protocol: "pancake"})
+  })
+
+//
+
+// 0x3fa2556c9466c9013d982967788dd1c20daff3e36703b492c5ac391cd842becf
