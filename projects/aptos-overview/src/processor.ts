2bd5952cccc204a0e9c18672e4517c9a32f3234e
Fuyao Zhao <zfy0701@gmail.com>
2022-11-11 07:28:59 -0800

    update total txns

--- projects/aptos-overview/src/processor.ts
+++ projects/aptos-overview/src/processor.ts
@@ -11,6 +11,7 @@ import { getChainQueryClient } from "@sentio/sdk/lib/aptos/utils";
 import * as soffle3 from "./types/aptos/soffle3";
 import * as topaz from "./types/aptos/topaz";
 import * as bluemoves from "./types/aptos/bluemoves";
+import { AptosResourceContext } from "@sentio/sdk/lib/aptos";
 
 const txnCounter = new Counter("txn_counter")
 
@@ -30,9 +31,6 @@ for (const x of DEFAULT_MAINNET_LIST) {
 //   .onEntryCreateResourceAccountAndPublishPackage((call, ctx) => {
 //     txnCounter.add(ctx, 1, { kind: "account_creation"})
 //   })
-
-const queryClient = getChainQueryClient()
-// await queryClient.aptosSQLQuery("asdfsf")
 
 managed_coin.bind()
   .onEntryRegister((call, ctx) => {
@@ -101,4 +99,37 @@ for (const m of [bluemoves.marketplaceV2, bluemoves.offer_lib]) {
     .onTransaction((tx, ctx) => {
       txnCounter.add(ctx, 1, { kind: "nft", protocol: "bluemoves"})
     })
+}
+
+const dailyTxn = new Gauge("txn");
+const queryClient = getChainQueryClient()
+
+aptos.AptosAccountProcessor.bind({address: "0x1"})
+    .onTimeInterval(async (resources, ctx) => sync(ctx),
+        24 * 60)
+
+async function sync(ctx: AptosResourceContext) {
+  interface myRow {
+    num_failed_txns: number;
+    num_successful_txns: number;
+    num_total_txns: number;
+  }
+  const sql = `SELECT  COUNT_IF(success=false) AS num_failed_txns,
+                     COUNT_IF(success=true) AS num_successful_txns,
+                     COUNT(*) AS num_total_txns 
+              FROM txns WHERE TIMESTAMP between ${(ctx.timestampInMicros - 86400000000).toString()}
+             AND ${ctx.timestampInMicros.toString()}`
+  // console.log(sql)
+  let ret = await queryClient.aptosSQLQuery({
+    sql: sql,
+    network: "aptos_mainnet",
+    arbitraryRange: true,
+  })
+  // console.log(ret)
+  for (let row of ret.documents) {
+    let obj: myRow = JSON.parse(row)
+    dailyTxn.record(ctx, obj.num_failed_txns, { kind: "failed"})
+    dailyTxn.record(ctx, obj.num_successful_txns, { kind: "successful"})
+    dailyTxn.record(ctx, obj.num_total_txns, { kind: "total"})
+  }
 }
