4af0dadfc1acc9f88f118295c16a96690176b2f8
TY <111388856+SentioSWE@users.noreply.github.com>
2022-11-11 20:07:34 +0800

    add more metrics from aptos network queries

--- projects/martian/src/processor.ts
+++ projects/martian/src/processor.ts
@@ -3,6 +3,14 @@ import { getChainQueryClient } from "@sentio/sdk/lib/aptos/utils";
 import {AptosResourceContext} from "@sentio/sdk/lib/aptos";
 
 const dailyTxn = new Gauge("txn");
+const accumTxn = new Counter("accum_txn");
+const dailyAverageTps = new Gauge("average_tps");
+
+const dau = new Gauge("dau");
+
+const dailyGas = new Gauge("gas")
+const accumGas = new Counter("accum_gas")
+
 const queryClient = getChainQueryClient()
 
 aptos.AptosAccountProcessor.bind({address: "0x1"})
@@ -14,12 +22,18 @@ async function sync(ctx: AptosResourceContext) {
         num_failed_txns: number;
         num_successful_txns: number;
         num_total_txns: number;
+        average_tps: number;
+        estimated_num_unique_users: number;
+        total_gas_price: number;
     }
-    let sql = `SELECT  COUNT_IF(success=false) AS num_failed_txns,
-                     COUNT_IF(success=true) AS num_successful_txns,
-                     COUNT(*) AS num_total_txns 
-              FROM txns WHERE TIMESTAMP between ${(ctx.timestampInMicros - 86400000000).toString()}
-             AND ${ctx.timestampInMicros.toString()}`
+    let sql = `SELECT COUNT_IF(success=false) AS num_failed_txns,
+               COUNT_IF(success=true) AS num_successful_txns,
+               COUNT(*) AS num_total_txns,
+               COUNT(*)/86400 AS average_tps,
+               APPROX_COUNT_DISTINCT(sender) AS estimated_num_unique_users,
+               SUM(gas_used) / POW(10, 5) AS total_gas_price
+               FROM txns WHERE TIMESTAMP between ${(ctx.timestampInMicros - 86400000000).toString()}
+               AND ${ctx.timestampInMicros.toString()}`
     console.log(sql)
     let ret = await queryClient.aptosSQLQuery({
         sql: sql,
@@ -32,5 +46,12 @@ async function sync(ctx: AptosResourceContext) {
         dailyTxn.record(ctx, obj.num_failed_txns, { kind: "failed"})
         dailyTxn.record(ctx, obj.num_successful_txns, { kind: "successful"})
         dailyTxn.record(ctx, obj.num_total_txns, { kind: "total"})
+        accumTxn.add(ctx, obj.num_failed_txns, { kind: "failed"})
+        accumTxn.add(ctx, obj.num_successful_txns, { kind: "successful"})
+        accumTxn.add(ctx, obj.num_total_txns, { kind: "total"})
+        dailyAverageTps.record(ctx, obj.average_tps)
+        dau.record(ctx, obj.estimated_num_unique_users)
+        dailyGas.record(ctx, obj.total_gas_price)
+        accumGas.add(ctx, obj.total_gas_price)
     }
 }
