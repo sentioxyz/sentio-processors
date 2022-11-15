e84d63d3a89777a3c867e566a8f5d6b5dc1e7cb3
Kan Qiao <qiaokan.buaa@gmail.com>
2022-11-15 08:22:34 -0800

    change price feed

--- common/src/aptos/coin.ts
+++ common/src/aptos/coin.ts
@@ -3,10 +3,12 @@ import { DEFAULT_MAINNET_LIST, RawCoinInfo } from "@manahippo/coin-list/dist/lis
 import { AptosClient } from "aptos-sdk";
 import { aptos, BigDecimal } from "@sentio/sdk";
 import { coin } from "@sentio/sdk/lib/builtin/aptos/0x1";
+import { getPriceClient } from '@sentio/sdk/lib/utils/price'
 import CoinInfo = coin.CoinInfo;
 import { getRandomInt } from "../utils";
 
 const client = new AptosClient("https://aptos-mainnet.nodereal.io/v1/0c58c879d41e4eab8fd2fc0406848c2b/")
+const priceClient = getPriceClient("http://test-web-server:10010")
 
 export interface BaseCoinInfoWithBridge extends RawCoinInfo {
   bridge: string
@@ -65,66 +67,29 @@ export function scaleDown(n: bigint, decimal: number) {
   return new BigDecimal(n.toString()).div(new BigDecimal(10).pow(decimal))
 }
 
-const priceCache = new Map<string, number>()
-const lastPriceCache = new Map<string, number>()
 
 export async function getPrice(coinType: string, timestamp: number) {
   if (!whiteListed(coinType)) {
     return 0.0
   }
-
-  const id = CORE_TOKENS.get(coinType)?.coingecko_id
-  if (!id) {
-    if (coinType === '0x881ac202b1f1e6ad4efcff7a1d0579411533f2502417a19211cfc49751ddb5f4::coin::MOJO') {
-      return 0.01618810
-    }
-    if (coinType === '0x5c738a5dfa343bee927c39ebe85b0ceb95fdb5ee5b323c95559614f5a77c47cf::Aptoge::Aptoge') {
-      return 0.271427
-    }
-
-    throw Error("can't find coin id" + coinType)
-  }
-
   const date = new Date(timestamp / 1000)
-  return getPriceByDate(coinType, id, date)
-}
-
-export async function getPriceByDate(coinType: string, id: string, date: Date): Promise<number> {
-  const dateStr =  [date.getUTCDate(), date.getUTCMonth()+1, date.getUTCFullYear()].join("-")
-
-  const cacheKey = id + dateStr
-  let price = priceCache.get(cacheKey)
-  if (price) {
-    return price
-  }
-  let res: any
-  while(true) {
-    try {
-      const requestUrl = `http://coingecko-proxy-server.chain-sync:8647/api/v3/coins/${id}/history?date=${dateStr}&localization=false&x_cg_pro_api_key=CG-NayGdpa2CqG1jp5CLtcA4kVp`
-      const response = await fetch(requestUrl)
-
-      res = await response.json()
-    } catch (e) {
-      await delay(1000)
-      continue
-    }
-    break
-  }
-  if (!res || !res.market_data || !res.market_data.current_price || !res.market_data.current_price.usd) {
-    console.error("no price data for ", coinType, id, dateStr)
-    price = lastPriceCache.get(id) || 0
-    if (!price) {
-      const previousDate = new Date()
-      previousDate.setDate(date.getDate() - 1);
-      price = await getPriceByDate(coinType, id, previousDate)
-      // console.error("can't even found last price", id, dateStr)
-    }
-  } else {
-    price = res.market_data.current_price.usd as number
+  let price : any
+  try {
+    const response = await priceClient.getPrice({
+      timestamp: date,
+      coinId: {
+        address: {
+          chain: "aptos_mainnet",
+          address: coinType,
+        }
+      }
+    })
+    price = response.price
+  } catch (e) {
+    console.log("error getting price", e, timestamp, coinType)
+    price = 0
   }
 
-  priceCache.set(cacheKey, price)
-  lastPriceCache.set(id, price)
   return price
 }
 
