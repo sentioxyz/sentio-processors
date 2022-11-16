690dceaa8c71305be835e48283df7e2f766a14be
Fuyao Zhao <zfy0701@gmail.com>
2022-11-16 10:08:03 -0800

    more pyth details

--- projects/pyth/src/processor.ts
+++ projects/pyth/src/processor.ts
@@ -3,13 +3,15 @@ import { Counter, Gauge } from "@sentio/sdk";
 import { toBigDecimal } from "@sentio/sdk/lib/utils/conversion";
 import { PRICE_MAP } from "./pyth";
 import { BigDecimal } from "@sentio/sdk/lib/core/big-decimal";
+import { console_v1 } from "./types/aptos/pyth2";
 
 const commonOptions = { sparse: true }
 const priceGauage = new Gauge("price", commonOptions)
 const priceEMAGauage = new Gauge("price_ema", commonOptions)
 
 const updates = new Counter("update")
-const messages = new Counter("messages")
+const messages = new Counter("update_price_feeds_with_funder")
+const messages2 = new Counter("mint_with_pyth_and_price")
 
 event.bind()
   .onEventPriceFeedUpdate((evt, ctx) => {
@@ -26,6 +28,10 @@ pyth.bind()
   .onEntryUpdatePriceFeedsWithFunder((call, ctx) => {
     messages.add(ctx, 1)
   })
+
+console_v1.bind().onEntryMintWithPythAndPrice((evt, ctx) => {
+  messages2.add(ctx, 1)
+})
 
 function getPrice(p: price.Price) {
   let expo = toBigDecimal(p.expo.magnitude)
