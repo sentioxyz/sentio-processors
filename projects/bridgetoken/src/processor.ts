ef31a8186b7ae820818dc4496b65e998c85dc770
Fuyao Zhao <zfy0701@gmail.com>
2022-12-05 16:42:27 -0800

    imrpove bridge token

--- projects/bridgetoken/src/processor.ts
+++ projects/bridgetoken/src/processor.ts
@@ -5,8 +5,15 @@ import {
   getPrice,
   scaleDown
 } from "@sentio-processor/common/dist/aptos";
-import { aptos, Counter, Gauge } from "@sentio/sdk";
-import { aggregator, coin, managed_coin, optional_aggregator } from "@sentio/sdk/lib/builtin/aptos/0x1";
+import { AccountEventTracker, aptos, Counter, Gauge } from "@sentio/sdk";
+import {
+  aggregator,
+  coin,
+  account,
+  managed_coin,
+  optional_aggregator,
+  type_info
+} from "@sentio/sdk/lib/builtin/aptos/0x1";
 import { delay, getRandomInt } from "@sentio-processor/common/dist";
 import type { Transaction_UserTransaction, TransactionPayload_EntryFunctionPayload } from 'aptos-sdk/src/generated'
 import { getPriceByType } from "@sentio/sdk/lib/utils/price";
@@ -14,8 +21,10 @@ import { APTOS_MAINNET_ID } from "@sentio/sdk/lib/utils/chain";
 import { AptosClient } from "aptos-sdk";
 import { TYPE_REGISTRY } from "@sentio/sdk/lib/aptos";
 
-const accounts = Counter.register("account", { sparse: true })
+const accounts = Counter.register("account", { sparse: false })
 const accountBalance = Gauge.register("account_balance", { sparse: true })
+
+const accountTracer = AccountEventTracker.register("register_coin_events")
 
 const BRIDGE_TOKENS = new Map<string, BaseCoinInfoWithBridge>()
 const PRICES = new Map<string, number>()
@@ -35,7 +44,7 @@ for (const token of CORE_TOKENS.values()) {
   })
 }
 
-const client = new AptosClient("https://aptos-mainnet.nodereal.io/v1/0c58c879d41e4eab8fd2fc0406848c2b/v1")
+const client = new AptosClient("http://aptos-mainnet-node-http.nodes:8080")
 
 //
 // coin.bind().onEventDepositEvent(async (evt, ctx) => {
@@ -72,34 +81,61 @@ const client = new AptosClient("https://aptos-mainnet.nodereal.io/v1/0c58c879d41
 
 
 TYPE_REGISTRY.load(coin.ABI)
-managed_coin.bind()
-    .onEntryRegister(async (call, ctx) => {
-      const token = BRIDGE_TOKENS.get(call.type_arguments[0])
-      if (!token) {
-        console.log("skip")
-        return
-      }
-      ctx.logger.info("account_creation", {token: token.symbol, bridge: token.bridge, account:ctx.transaction.sender})
+account.bind().onEventCoinRegisterEvent(async (call, ctx) => {
+    // .onEntryRegister(async (call, ctx) => {
+    const type = extractTypeName(call.data_typed.type_info)
+  const accountAddress  =  call.guid.account_address
+    const token = BRIDGE_TOKENS.get(type)
+    if (!token) {
+      // console.log("skip")
+      return
+    }
+    // ctx.logger.info("account_creation", {token: token.symbol, bridge: token.bridge, account:ctx.transaction.sender})
 
-      const coinStore = `0x1::coin::CoinStore<${token.token_type.type}>`;
+    const coinStore = `0x1::coin::CoinStore<${token.token_type.type}>`;
 
-      const res = await client.getAccountResource(ctx.transaction.sender, coinStore)
-      const decodedRes = TYPE_REGISTRY.decodeResource<coin.CoinStore<any>>(res)
-      if (!decodedRes) {
-        console.log(res)
-        process.exit(1)
-        return
-      }
-      const amount = scaleDown(decodedRes.data_typed.coin.value, token.decimals)
-      const value = amount.multipliedBy(PRICES.get(token.token_type.type)!)
+    const res = await client.getAccountResource(accountAddress, coinStore)
+    const decodedRes = TYPE_REGISTRY.decodeResource<coin.CoinStore<any>>(res)
+    if (!decodedRes) {
+      console.log(res)
+      process.exit(1)
+      return
+    }
+    const amount = scaleDown(decodedRes.data_typed.coin.value, token.decimals)
+    const value = amount.multipliedBy(PRICES.get(token.token_type.type)!)
 
-      accounts.add(ctx, 1, {token: token.symbol, condition: "all"})
-      if (value.isGreaterThan(0)) {
-        accounts.add(ctx, 1, {token: token.symbol, condition: "gt_0"})
-      }
-      if (value.isGreaterThan(10)) {
-        accounts.add(ctx, 1, {token: token.symbol, condition: "gt_10"})
-      }
+    if (token.symbol ==='SOL') {
+      accountTracer.trackEvent(ctx, { distinctId: accountAddress})
+    }
 
-      // accounts.add(ctx, 1, { kind: "account_creation", token: call.type_arguments[0] })
-    })
+    accounts.add(ctx, 1, {token: token.symbol, condition: "all", bridge: token.bridge})
+    if (value.isGreaterThan(0)) {
+      accounts.add(ctx, 1, {token: token.symbol, condition: "gt_0", bridge: token.bridge})
+    }
+    if (value.isGreaterThan(10)) {
+      accounts.add(ctx, 1, {token: token.symbol, condition: "gt_10", bridge: token.bridge})
+    }
+
+    // accounts.add(ctx, 1, { kind: "account_creation", token: call.type_arguments[0] })
+  })
+
+function extractTypeName(typeInfo: type_info.TypeInfo) {
+  return [typeInfo.account_address, hex_to_ascii(typeInfo.module_name), hex_to_ascii(typeInfo.struct_name)].join("::")
+  // if (rawName.startsWith("Coin<")) {
+  //   return rawName.substring(5, rawName.length - 1)
+  // } else {
+  //   return rawName
+  // }
+}
+
+function hex_to_ascii(str1: String) {
+  var hex  = str1.toString();
+  if (hex.startsWith("0x")) {
+    hex = hex.substring(2)
+  }
+  var str = '';
+  for (var n = 0; n < hex.length; n += 2) {
+    str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+  }
+  return str;
+}
