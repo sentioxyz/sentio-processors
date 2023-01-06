47c97c841d2b8a15f58118f9bb253d16f0be9dff
Fuyao Zhao <zfy0701@gmail.com>
2023-01-06 02:44:16 -0800

    update sdk

--- projects/pyth-solana/src/processor.test.ts
+++ projects/pyth-solana/src/processor.test.ts
@@ -1,7 +1,7 @@
 import { TestProcessorServer } from '@sentio/sdk/lib/testing'
 import { BorshInstructionCoder, Idl } from "@project-serum/anchor";
 import { pyth_oracle_idl } from "./types/solana/pyth_oracle";
-import bs58 from "bs58";
+// import bs58 from "bs58";
 import { PythOracleCoder } from '@pythnetwork/client';
 import { PythOracle } from '@pythnetwork/client/lib/anchor';
 
@@ -13,13 +13,15 @@ describe('Test Processor', () => {
   })
 
   test('has valid config', async () => {
-    // // const instructionCoder = new BorshInstructionCoder(pyth_oracle_idl as Idl)
+    // const instructionCoder = new BorshInstructionCoder(pyth_oracle_idl as Idl)
     // const coder = new PythOracleCoder(pyth_oracle_idl as PythOracle);
     // const instructionCoder = coder.instruction
     //
     // const tt = bs58.decode("6mJFQCt94hG4CKNYKgVcwfDH6kX6Cb1uK8MxXBk9iAFmcdgsTYryFV")
     // // const instructionCoder = new BorshInstructionCoder(pyth_oracle_idl as Idl)
-    // const decodedIns = instructionCoder.decode("020000000d000000000000000000000000000000000000000000000000000000acfc2a0a00000000", "hex")
+    // let decodedIns = instructionCoder.decode("6mJFQCt94hG4CKNYKgVcwfDH6kX6Cb1uK8MxXBk9iAFmcdgsTYryFV", "base58")
+
+    // decodedIns = instructionCoder.decode(Buffer.from(bs58.decode("6mJFQCt94hG4CKNYKgVcwfDH6kX6Cb1uK8MxXBk9iAFmcdgsTYryFV")))
 
     // https://explorer.solana.com/tx/65CGX2dDXpsrvJZPc243yC9P666Eqh755d4KtgYAqeqfjPfpQrhj7J75mEt2Laefq68CrUvthk4nmYDHcDPLRn3H
     // https://github.com/pyth-network/pyth-sdk-rs/blob/main/pyth-sdk-solana/src/state.rs#L127
--- projects/pyth-solana/src/processor.ts
+++ projects/pyth-solana/src/processor.ts
@@ -3,26 +3,27 @@
 import { PythOracleProcessor } from "./types/solana/pyth_oracle_processor";
 import { Instruction } from "@project-serum/anchor";
 import { pyth_oracle_idl } from "./types/solana/pyth_oracle";
-import bs58 from "bs58";
+// import bs58 from "bs58";
 import { PythOracleCoder } from "@pythnetwork/client";
 import { PythOracle } from "@pythnetwork/client/lib/anchor";
 import { SolanaBindOptions } from "@sentio/sdk-solana";
+import { PythOracleInstructionCoder } from "@pythnetwork/client/lib/anchor/coder/instructions";
 
 // https://explorer.solana.com/tx/65CGX2dDXpsrvJZPc243yC9P666Eqh755d4KtgYAqeqfjPfpQrhj7J75mEt2Laefq68CrUvthk4nmYDHcDPLRn3H
 // Solana Mainetnet
 
-class CompatiblePythOracleProcessor extends PythOracleProcessor {
-  coder = new PythOracleCoder(pyth_oracle_idl as PythOracle);
-  decodeInstruction: (rawInstruction: string) => Instruction | null = (rawInstruction) => {
-    return this.coder.instruction.decode(Buffer.from(bs58.decode(rawInstruction)))
-  }
-  static bind(options: SolanaBindOptions): PythOracleProcessor {
-    if (options && !options.name) {
-      options.name = 'PythOracle'
-    }
-    return new CompatiblePythOracleProcessor(options)
-  }
-}
+// class CompatiblePythOracleProcessor extends PythOracleProcessor {
+//   coder = new PythOracleCoder(pyth_oracle_idl as PythOracle);
+//   decodeInstruction: (rawInstruction: string) => Instruction | null = (rawInstruction) => {
+//     return this.coder.instruction.decode(Buffer.from(bs58.decode(rawInstruction)))
+//   }
+//   static bind(options: SolanaBindOptions): PythOracleProcessor {
+//     if (options && !options.name) {
+//       options.name = 'PythOracle'
+//     }
+//     return new CompatiblePythOracleProcessor(options)
+//   }
+// }
 
 // PythOracleProcessor.bind({
 //   network: CHAIN_IDS.SOLANA_PYTH,
@@ -32,10 +33,11 @@ class CompatiblePythOracleProcessor extends PythOracleProcessor {
 //   console.log(args, accounts, ctx);
 // })
 
-CompatiblePythOracleProcessor.bind({
+PythOracleProcessor.bind({
   address: "FsJ3A3u2vn5cTVofAjvy6y5kwABJAqYWpe4975bi2epH",
+  instructionCoder: new PythOracleInstructionCoder(pyth_oracle_idl as PythOracle),
 }).onUpdPrice((args, accounts, ctx) => {
-  ctx.meter.Gauge("price").record(args.price, { price_account: accounts[1] })
+  ctx.meter.Gauge("price").record(args.price, { price_account: accounts.priceAccount })
 }).onUpdPriceNoFailOnError((args, accounts, ctx) => {
   console.log(args, accounts, ctx);
 })
