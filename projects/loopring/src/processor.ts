1b9513519b2952cc1572354ee7d13bcdf2e3052f
Kan Qiao <qiaokan.buaa@gmail.com>
2023-01-16 10:42:33 -0800

    add logs

--- projects/loopring/src/processor.ts
+++ projects/loopring/src/processor.ts
@@ -27,6 +27,7 @@ import { toBigDecimal } from "@sentio/sdk/lib/utils/conversion"
 import {deposit, withdraw} from "./metrics";
 import { RichClientError } from "nice-grpc-error-details";
 import { Status, ClientError } from "nice-grpc-common";
+import {INFO} from "@sentio/loopring-protocols/src/logs";
 
 GenericProcessor.bind(EVENT1, {address: LOOPRING_WALLET_MODULE}).onAllEvents(walletCounter)
 GenericProcessor.bind(EVENT, {address: LOOPRING_WALLET_FACTORY1}).onAllEvents(walletCounter)
@@ -66,14 +67,11 @@ const tvl = async function (_: any, ctx: ExchangeV3Context) {
     try {
       price = await getPriceByType("ethereum_mainnet", TOKEN_ARRAY[i], ctx.timestamp)
     } catch (error) {
-      if (error instanceof ClientError && error.code === Status.NOT_FOUND) {
-       continue
-      }
-      throw error
+        if (error instanceof ClientError && error.code === Status.NOT_FOUND) {
+            continue
+        }
+        throw error
     }
-    console.log("price", price, "scaledAmount", scaledAmount.toNumber(),
-        "scaledAmount", amount.toString(), "tokenInfo", tokenInfo.symbol,
-        "block", Number(ctx.blockNumber), "timestamp", ctx.timestamp, "address", TOKEN_ARRAY[i])
     ctx.meter.Gauge("tvl").record(scaledAmount.multipliedBy(price),
         {token: tokenInfo.symbol, address: TOKEN_ARRAY[i]})
     ctx.meter.Counter("tvl_count").add(1)
@@ -88,8 +86,8 @@ ExchangeV3Processor.bind({address: LOOPRING_EXCHANGE})
       if (call.error) {
         return
       }
+      ctx.logger.info(`SubmitBlocks triggered at ${ctx.blockNumber}`)
       ctx.meter.Counter("submit_block").add(1)
-      // console.log(ctx.contract.provider)
       const tx = await ctx.contract.provider.getTransaction(call.transactionHash)
       const gasPrice = tx.gasPrice
       const gasUsedFromCall = call.result.gasUsed
@@ -98,10 +96,8 @@ ExchangeV3Processor.bind({address: LOOPRING_EXCHANGE})
       if (gasPrice !== undefined) {
         const gasSpent = toBigDecimal(gasUsed).multipliedBy(token.scaleDown(gasPrice!, 18))
         const gasSpent2 = new BigDecimal(gasUsedFromCall).multipliedBy(token.scaleDown(gasPrice!, 18))
-
         ctx.meter.Counter("eth_spent_on_gas").add(gasSpent)
         ctx.meter.Counter("eth_spent_on_gas2").add(gasSpent2)
-
       }
       for (const block of call.args.blocks) {
         processBlockStruct(block, call.transactionHash, ctx)
@@ -117,6 +113,7 @@ async function depositGauge(event: DepositRequestedEvent, ctx: ExchangeV3Context
   const amount = await scaleDown(event.args.amount, tokenInfo.decimal)
 
   if (!tokenInfo.symbol.startsWith("LP-")) {
+    ctx.logger.info(`Deposit ${amount} ${tokenInfo.symbol} at ${ctx.blockNumber}`)
     deposit.record(ctx, amount, {tokenId: tokenInfo.symbol, address: event.args.token})
   }
 }
@@ -126,6 +123,7 @@ async function withdrawGauge(event: WithdrawalCompletedEvent, ctx: ExchangeV3Con
   const amount = await scaleDown(event.args.amount, tokenInfo.decimal)
 
   if (!tokenInfo.symbol.startsWith("LP-")) {
+    ctx.logger.info(`Withdraw ${amount} ${tokenInfo.symbol} at ${ctx.blockNumber}`)
     withdraw.record(ctx, amount, {token: tokenInfo.symbol, address: event.args.token})
   }
 }
