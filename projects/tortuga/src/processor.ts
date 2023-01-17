c7a3aaca0eafc0951e336f6f1685e4a15e612b16
Fuyao Zhao <zfy0701@gmail.com>
2023-01-17 11:40:12 -0800

    add supply for tortuga

--- projects/tortuga/src/processor.ts
+++ projects/tortuga/src/processor.ts
@@ -5,8 +5,12 @@ import { liquidity_pool } from "./types/aptos/liquidswap";
 import { stake_router } from "./types/aptos/tortuga";
 import { toBigDecimal } from "@sentio/sdk/lib/utils/conversion";
 import { BigDecimal } from "@sentio/sdk/lib/core/big-decimal";
+import { AptosAccountProcessor, TYPE_REGISTRY, TypedMoveResource } from "@sentio/sdk-aptos";
+import { coin, optional_aggregator } from "@sentio/sdk-aptos/lib/builtin/0x1";
 
 const commonOptions = { sparse:  false }
+
+const supply = new Gauge("total_supply", commonOptions)
 
 const liquidityAdd = new Counter("liquidity_add_num", commonOptions)
 const liquidityRemoved = new Counter("liquidity_remove_num", commonOptions)
@@ -28,6 +32,7 @@ const accountTracker = AccountEventTracker.register("users")
 
 const APT = '0x1::aptos_coin::AptosCoin'
 const tAPT = '0x84d7aeef42d38a5ffc3ccef853e1b82e4958659d16a7de736a29c55fbbeb0114::staked_aptos_coin::StakedAptosCoin'
+const coinInfoType = `0x1::coin::CoinInfo<${tAPT}>`
 
 // tortuga
 stake_router.bind()
@@ -41,7 +46,7 @@ stake_router.bind()
       lastStakeAmount.record(ctx, scaleDown(evt.data_typed.t_apt_coins), { coin: "tAPT"})
     }
     if (amount.gt(1000)) {
-      ctx.logger.info("stake " + amount + " APT", {amount: amount, whale: amount.gt(1000)})
+      ctx.logger.info("stake " + amount + " APT", {amount: amount})
     }
     stake.add(ctx, 1)
   })
@@ -51,7 +56,7 @@ stake_router.bind()
     unstakeAmount.add(ctx, amount, { coin: "APT"})
     unstakeAmount.add(ctx, scaleDown(evt.data_typed.t_apt_coins), { coin: "tAPT"})
     if (amount.gt(1000) ) {
-      ctx.logger.info("unstake apt " + amount + "APT", {amount: amount, whale: amount.gt(1000)})
+      ctx.logger.info("unstake apt " + amount + "APT", {amount: amount})
     }
     unstake.add(ctx, 1)
   })
@@ -124,6 +129,26 @@ liquidity_pool.bind({startVersion: 299999})
       vol.record(ctx, scaleDown(evt.data_typed.y_in + evt.data_typed.y_out), { coin: getSymbol(evt.type_arguments[1]), protocol: "liquidswap"})
     }
   })
+
+AptosAccountProcessor.bind({address: "0x84d7aeef42d38a5ffc3ccef853e1b82e4958659d16a7de736a29c55fbbeb0114"})
+    .onTimeInterval(async (resources, ctx) => {
+      const coinInfoRes = TYPE_REGISTRY.filterAndDecodeResources<coin.CoinInfo<any>>(coin.CoinInfo.TYPE_QNAME, resources)
+      if (coinInfoRes.length === 0) {
+        return
+      }
+      const coinInfo = coinInfoRes[0].data_typed
+
+      const aggOption = (coinInfo.supply.vec as optional_aggregator.OptionalAggregator[])[0]
+      if (aggOption.integer.vec.length) {
+        const intValue = (aggOption.integer.vec[0] as optional_aggregator.Integer)
+
+        supply.record(ctx, scaleDown(intValue.value))
+      } else {
+        console.error("no supply for tAPT found")
+      }
+
+    }, 60, 12 * 60, coinInfoType)
+
 
 function isAptTAptPair(coinx: string, coiny: string): boolean {
   if (coinx === tAPT && coiny === APT) {
