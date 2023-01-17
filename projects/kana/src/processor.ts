4d7a01043117789d2d46f0d3ed9219dad3402bb4
ye-sentio <ye@sentio.xyz>
2023-01-17 20:19:34 +0800

    revised swapstep metrics

--- projects/kana/src/processor.ts
+++ projects/kana/src/processor.ts
@@ -1,112 +1,106 @@
-import { KanalabsAggregatorV1, KanalabsBridgeV1, KanalabsRouterV1, KanalabsStakingV1 } from './types/aptos/KanalabsAggregatorV1'
-import { KanaBridge, KanaBridgeV1, aggregatorV1, AggregatorTestV1, KanaAggregatorV1, kana_aggregatorv1 } from './types/aptos/KanalabsV0'
+import { kana_aggregatorv1 } from './types/aptos/KanalabsV0'
+import { getPrice, getCoinInfo, whiteListed, scaleDown } from "@sentio-processor/common/dist/aptos/coin"
+import { AccountEventTracker, Counter, Gauge } from "@sentio/sdk";
+import { type_info } from "@sentio/sdk-aptos/lib/builtin/0x1"
 
-KanalabsAggregatorV1.bind()
-  .onEntrySwap((call, ctx) => {
-    ctx.meter.Counter('swap_token_entry').add(1)
-    console.log('test0')
-  })
-  .onEntryAdvancedRoute((call, ctx) => {
-    ctx.meter.Counter('advance_entry').add(1)
-    console.log('test1')
-  })
-  .onEntryDirectRoute((call, ctx) => {
-    ctx.meter.Counter('direct_entry').add(1)
-    console.log('test2')
-  })
-  .onEventSwapCount((call, ctx) => {
-    ctx.meter.Counter('swap_count_event').add(1)
-    console.log('test3')
-  })
-  .onEventSwapStepEvent((call, ctx) => {
-    ctx.meter.Counter('swap_step_event').add(1)
-    console.log('test4')
-  })
-  .onEntryInitPlatformProfile((call, ctx) => {
-    ctx.meter.Counter('init').add(1)
-    console.log('test_init')
-  })
+const commonOptions = { sparse: false }
+export const volOptions = {
+  sparse: true,
+  aggregationConfig: {
+    intervalInMinutes: [60],
+    // discardOrigin: false
+  }
+}
 
-KanalabsStakingV1.bind()
-  .onEntryStake((call, ctx) => {
-    ctx.meter.Counter('stake_entry').add(1)
-    console.log('test5')
-  })
-  .onEventStakeCount((call, ctx) => {
-    ctx.meter.Counter('stake_count_event').add(1)
-    console.log('test6')
-  })
-
-KanalabsRouterV1.bind()
-  .onEntryRoute((call, ctx) => {
-    ctx.meter.Counter('route_entry').add(1)
-    console.log('test7')
-  })
-  .onEventRouteCount((call, ctx) => {
-    ctx.meter.Counter('route_count_event').add(1)
-    console.log('test8')
-  })
-
-
-KanalabsBridgeV1.bind()
-  .onEntryClaimTokens((call, ctx) => {
-    ctx.meter.Counter('claim_entry').add(1)
-    console.log('test9')
-  })
-  .onEntryTransferTokens((call, ctx) => {
-    ctx.meter.Counter('transfer_entry').add(1)
-    console.log('test10')
-  })
-  .onEventClaimCount((call, ctx) => {
-    ctx.meter.Counter('claim_event').add(1)
-    console.log('test11')
-  })
-  .onEventTransferCount((call, ctx) => {
-    ctx.meter.Counter('transfer_event').add(1)
-    console.log('test12')
-  })
-  .onEntryUpdatePlatformFeeAmount((call, ctx) => {
-    ctx.meter.Counter('update_fee_amt').add(1)
-    console.log('test_update_fee_amt')
-  })
-  .onEntryUpdatePlatformFeeCollect((call, ctx) => {
-    ctx.meter.Counter('update_fee_collect').add(1)
-    console.log('test_update_fee_collect')
-  })
-  .onEntryUpdatePlatformFeeReceiver((call, ctx) => {
-    ctx.meter.Counter('update_fee_receive').add(1)
-    console.log('test_update_fee_receive')
-  })
+const accountTracker = AccountEventTracker.register("users")
+const totalTx = new Counter("tx", commonOptions)
+const vol = Gauge.register("vol", volOptions)
 
 
 kana_aggregatorv1.bind()
-  .onEntryAdvancedRoute((call, ctx) => {
-    ctx.meter.Counter('advance_route_on_entry').add(1)
+  .onEventSwapStepEvent(async (event, ctx) => {
+    ctx.meter.Counter('swap_step_event_emit').add(1)
+    const dexType = event.data_typed.dex_type
+    const poolType = event.data_typed.pool_type
+    const inputAmount = event.data_typed.input_amount
+    const outputAmount = event.data_typed.output_amount
+    const xType = extractTypeName(event.data_typed.x_type_info)
+    const yType = extractTypeName(event.data_typed.y_type_info)
+
+    const timestamp = event.data_typed.time_stamp
+
+    const coinXInfo = getCoinInfo(xType)
+    const coinYInfo = getCoinInfo(yType)
+    const symbolX = coinXInfo.symbol
+    const symbolY = coinYInfo.symbol
+    const priceX = await getPrice(xType, Number(timestamp))
+    const priceY = await getPrice(yType, Number(timestamp))
+    const pair = constructPair(xType, yType)
+    const volume = scaleDown(inputAmount, coinXInfo.decimals).multipliedBy(priceX)
+    const displayPair = constructDisplay(symbolX, symbolY)
+
+    accountTracker.trackEvent(ctx, { distinctId: ctx.transaction.sender })
+    totalTx.add(ctx, 1)
+    if (whiteListed(xType)) {
+      vol.record(ctx, volume, { dex: getDex(dexType), poolType: poolType.toString(), xType: xType, yType: yType, symbolX: symbolX, symbolY: symbolY, pair: displayPair })
+    }
+
   })
-  .onEntryCreateAuxSigner((call, ctx) => {
-    ctx.meter.Counter('create_aux_on_entry').add(1)
-  })
-  .onEntryDirectRoute((call, ctx) => {
-    ctx.meter.Counter('direct_route_on_entry').add(1)
-  })
-  .onEntryIntermediateRoute((call, ctx) => {
-    ctx.meter.Counter('intermediate_route_on_entry').add(1)
-  })
-  .onEntrySetPlatformProfile((call, ctx) => {
-    ctx.meter.Counter('set_platform_profile_on_entry').add(1)
-  })
-  .onEntrySetReferralProfile((call, ctx) => {
-    ctx.meter.Counter('set_referral_profile_on_entry').add(1)
-  })
-  .onEntryUpdatePlatformFeeAmount((call, ctx) => {
-    ctx.meter.Counter('update_platform_fee_on_entry').add(1)
-  })
-  .onEntryUpdatePlatformFeeReceiver((call, ctx) => {
-    ctx.meter.Counter('update_fee_receiver_on_entry').add(1)
-  })
-  .onEventSwapCount((call, ctx) => {
-    ctx.meter.Counter('swap_count_on_event').add(1)
-  })
-  .onEventSwapStepEvent((call, ctx) => {
-    ctx.meter.Counter('swap_step_on_event').add(1)
-  })
+
+function constructPair(xType: String, yType: String) {
+  if (xType > yType) {
+    return xType + "-" + yType
+  } else {
+    return yType + "-" + xType
+  }
+}
+
+function constructDisplay(symbolX: String, symbolY: String) {
+  if (symbolX > symbolY) {
+    return symbolX + "-" + symbolY
+  } else {
+    return symbolY + "-" + symbolX
+  }
+}
+
+function extractTypeName(typeInfo: type_info.TypeInfo) {
+  var rawName = hex_to_ascii(typeInfo.struct_name)
+  if (rawName.startsWith("Coin<")) {
+    return rawName.substring(5, rawName.length - 1)
+  } else {
+    return rawName
+  }
+}
+
+function hex_to_ascii(str1: String) {
+  var hex = str1.toString();
+  if (hex.startsWith("0x")) {
+    hex = hex.substring(2)
+  }
+  var str = '';
+  for (var n = 0; n < hex.length; n += 2) {
+    str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+  }
+  return str;
+}
+
+
+function getDex(dex: number) {
+  if (DEX_MAP.has(dex)) {
+    return DEX_MAP.get(dex)!
+  } else {
+    return 'dex_unk_' + dex.toString()
+  }
+}
+
+const DEX_MAP = new Map<number, string>([
+  [1, 'Liquidswap'],
+  [2, 'Aptoswap'],
+  [3, 'Basiq'],
+  [4, 'PancakeSwap'],
+  [5, 'Cetus'],
+  [6, 'Animeswap'],
+  [7, 'Aux'],
+  [8, 'Orbic']
+])
+
