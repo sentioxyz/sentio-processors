49f3a06f14986239d3e416ed5e66742a9cb4d721
Kan Qiao <qiaokan.buaa@gmail.com>
2023-01-27 17:24:43 -0800

    initial version of uniswap top 50 pools volume

--- projects/uniswap/src/processor.ts
+++ projects/uniswap/src/processor.ts
@@ -1,71 +1,141 @@
 import {SwapEvent, UniswapContext, UniswapProcessor, UniswapProcessorTemplate} from './types/uniswap'
 import {PoolCreatedEvent, UniswapFactoryContext, UniswapFactoryProcessor} from "./types/uniswapfactory";
+import { token, conversion  } from "@sentio/sdk/lib/utils"
+import type { BaseContract, BigNumber } from 'ethers'
+import { ERC20Context, ERC20Processor, getERC20Contract } from '@sentio/sdk/lib/builtin/erc20'
+import {scaleDown} from "@sentio-processor/common/dist/aptos";
+import {getPriceByType} from "@sentio/sdk/lib/utils/price"
+import { RichClientError } from "nice-grpc-error-details";
+import { Status, ClientError } from "nice-grpc-common";
+import {Gauge, MetricOptions} from "@sentio/sdk";
 
 
-const TokenWatching = new Map([
-    ['0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 'USDC'],
-    ['0x6B175474E89094C44Da98b954EedeAC495271d0F', 'DAI']
-])
+const poolWatching = [
+"0x5777d92f208679db4b9778590fa3cab3ac9e2168",
+"0x6c6bc977e13df9b0de53b251522280bb72383700",
+"0xcbcdf9626bc03e24f779434178a73a0b4bad62ed",
+"0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640",
+"0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8",
+"0x4585fe77225b41b697c938b018e2ac67ac5a20c0",
+"0x8ee3cc8e29e72e03c4ab430d7b7e08549f0c71cc",
+"0xc63b0708e2f7e69cb8a1df0e1389a98c35a76d52",
+"0x4e68ccd3e89f51c3074ca5072bbac773960dfa36",
+"0x7379e81228514a1d2a6cf7559203998e20598346",
+"0x3416cf6c708da44db2624d63ea0aaef7113527c6",
+"0x1d42064fc4beb5f8aaf85f4617ae8b3b5b8bd801",
+"0xe0554a476a092703abdb3ef35c80e0d76d32939f",
+"0x5c128d25a21f681e678cb050e551a895c9309945",
+"0x11b815efb8f581194ae79006d24e0d814b7697f6",
+"0x00cef0386ed94d738c8f8a74e8bfd0376926d24c",
+"0x99ac8ca7087fa4a2a1fb6357269965a2014abc35",
+"0x97e7d56a0408570ba1a7852de36350f7713906ec",
+"0xc2e9f25be6257c210d7adf0d4cd6e3e881ba25f8",
+"0x64a078926ad9f9e88016c199017aea196e3899e1",
+"0x40e629a26d96baa6d81fae5f97205c2ab2c1ff29",
+"0x840deeef2f115cf50da625f7368c24af6fe74410",
+"0xa6cc3c2531fdaa6ae1a3ca84c2855806728693e8",
+"0xf56d08221b5942c428acc5de8f78489a97fc5599",
+"0x735a26a57a0a0069dfabd41595a970faf5e1ee8b",
+"0x290a6a7460b308ee3f19023d2d00de604bcf5b42",
+"0x1c5c60bef00c820274d4938a5e6d04b124d4910b",
+"0x39529e96c28807655b5856b3d342c6225111770e",
+"0xa3f558aebaecaf0e11ca4b2199cc5ed341edfd74",
+"0x7858e59e0c01ea06df3af3d20ac7b0003275d4bf",
+"0xe8c6c9227491c0a8156a0106a0204d881bb7e531",
+"0x7bea39867e4169dbe237d55c8242a8f2fcdcc387",
+"0x60594a405d53811d3bc4766596efd80fd545a270",
+"0xac4b3dacb91461209ae9d41ec517c2b9cb1b7daf",
+"0xb9044f46dcdea7ecebbd918a9659ba8239bd9f37",
+"0xc2a856c3aff2110c1171b8f942256d40e980c726",
+"0xa4e0faa58465a2d369aa21b3e42d43374c6f9613",
+"0xf482fce04ef6f29ad56e46fef2de038c42126e2e",
+"0x87986ae1e99f99da1f955d16930dc8914ffbed56",
+"0x69d91b94f0aaf8e8a2586909fa77a5c2c89818d5",
+"0xf4ad61db72f114be877e87d62dc5e7bd52df4d9b",
+"0x4b5ab61593a2401b1075b90c04cbcdd3f87ce011",
+"0x9febc984504356225405e26833608b17719c82ae",
+"0x92560c178ce069cc014138ed3c2f5221ba71f58a",
+"0x2f62f2b4c5fcd7570a709dec05d68ea19c82a9ec",
+"0x5764a6f2212d502bc5970f9f129ffcd61e5d7563"]
 
-function checkTokensWatching(token0: string, token1: string): boolean {
-    return TokenWatching.get(token0) !== undefined && TokenWatching.get(token1) != undefined
+async function getTokenInfo(address: string): Promise<token.TokenInfo> {
+    if (address !== "0x0000000000000000000000000000000000000000") {
+        return await token.getERC20TokenInfo(address)
+    } else {
+        return token.NATIVE_ETH
+    }
 }
 
-const poolTemplate = new UniswapProcessorTemplate()
-    .onEventSwap(
+export const gaugeOptions: MetricOptions = {
+    sparse: true,
+    aggregationConfig: {
+        intervalInMinutes: [60],
+    }
+}
+
+interface poolInfo {
+    token0: string
+    token1: string
+    fee: string
+}
+
+// define a map from string to poolInfo
+let poolInfoMap = new Map<string, poolInfo>()
+
+export const vol = Gauge.register("vol", gaugeOptions)
+
+for (let i=0;i<poolWatching.length;i++) {
+    let address = poolWatching[i]
+    UniswapProcessor.bind({address: address}).onEventSwap(
         async function (event: SwapEvent, ctx: UniswapContext) {
-            ctx.meter.Gauge("swap_amount0").record(
-                Math.abs(Number(event.args.amount0.toBigInt())),
+            let token0 : any
+            let token1 : any
+            let fee : any
+            let localAddress = ctx.address
+            console.log("poolInfoMap for " + localAddress + " is " + poolInfoMap.has(localAddress))
+            if (!poolInfoMap.has(localAddress)) {
+                token0 = await ctx.contract.token0()
+                token1 = await ctx.contract.token1()
+                fee = await ctx.contract.fee()
+                poolInfoMap.set(localAddress, {token0: token0.toString(),
+                    token1: token1.toString(), fee: fee.toString()})
+                console.log("set poolInfoMap for " + localAddress)
+            } else {
+                token0 = poolInfoMap.get(localAddress)?.token0
+                token1 = poolInfoMap.get(localAddress)?.token1
+                fee = poolInfoMap.get(localAddress)?.fee
+            }
+
+            let [token0Info, token0Amount] = await getTokenDetails(ctx, token0.toString(), event.args.amount0)
+            let [token1Info, token1Amount] = await getTokenDetails(ctx, token1.toString(), event.args.amount1)
+
+            vol.record(ctx,
+                token0Amount.abs(),
                 {
-                    from: TokenWatching.get(await ctx.contract.token0()) || "",
-                    to: TokenWatching.get(await ctx.contract.token1()) || "",
-                    fee: (await ctx.contract.fee()) + ""
+                    from: token0.toString(),
+                    to: token1.toString(),
+                    fee: fee.toString(),
                 }
             )
         }
     )
-
-UniswapFactoryProcessor.bind({address: '0x1F98431c8aD98523631AE4a59f267346ea31F984'})
-    .onEventPoolCreated(
-        async function (event: PoolCreatedEvent, ctx: UniswapFactoryContext) {
-            ctx.meter.Counter('pool_num').add(1)
-
-            if (checkTokensWatching(event.args.token0, event.args.token1)) {
-                ctx.meter.Counter('watching_pool_num').add(1)
-                poolTemplate.bind({address: event.args.pool, startBlock: ctx.blockNumber})
-            }
+}
+async function getTokenDetails(ctx: UniswapContext, address: string, amount: BigNumber):
+    Promise<[token.TokenInfo, BigNumber]> {
+    const tokenInfo = await getTokenInfo(address)
+    let scaledAmount = scaleDown(amount.toBigInt(), tokenInfo.decimal)
+    let price : any
+    try {
+        price = await getPriceByType("1", address, ctx.timestamp)
+    } catch (error) {
+        if (error instanceof ClientError && error.code === Status.NOT_FOUND) {
+            price = 0
+            return [tokenInfo, price]
         }
-    )
-
-UniswapProcessor.bind({address: '0x5777d92f208679db4b9778590fa3cab3ac9e2168'})
-    .onEventSwap(
-        async function (event: SwapEvent, ctx: UniswapContext) {
-            ctx.meter.Gauge('dai_usdc_amount0').record(Math.abs(Number(event.args.amount0.toBigInt())))
-            ctx.meter.Gauge('dai_usdc_amount1').record(Math.abs(Number(event.args.amount1.toBigInt())))
-        }
-    )
+        throw error
+    }
+    price = scaledAmount.multipliedBy(price)
+    return [tokenInfo, price]
+}
 
 
-// X2y2Processor.bind({address: '0xB329e39Ebefd16f40d38f07643652cE17Ca5Bac1', startBlock: 14211735}).onBlock(
-//     async function (_, ctx: X2y2Context) {
-//         const phase = (await ctx.contract.currentPhase()).toString()
-//         const reward = Number((await ctx.contract.rewardPerBlockForStaking()).toBigInt() / 10n ** 18n)
-//         ctx.meter.Gauge('reward_per_block').record(reward, {phase})
-//     }
-// )
-//
-// export const filter = ERC20Processor.filters.Transfer(
-//     '0x0000000000000000000000000000000000000000',
-//     '0xb329e39ebefd16f40d38f07643652ce17ca5bac1'
-// )
-//
-// ERC20Processor.bind({address: '0x1e4ede388cbc9f4b5c79681b7f94d36a11abebc9'}).onEventTransfer(
-//     handleTransfer,
-//     filter // filter is an optional parameter
-// )
-//
-// async function handleTransfer(event: TransferEvent, ctx: ERC20Context) {
-//     const val = Number(event.args.value.toBigInt() / 10n ** 18n)
-//     ctx.meter.Counter('token').add(val)
-// }
-//
