e1549142b841a609d536e518e2f75b1210da4c87
Fuyao Zhao <zfy0701@gmail.com>
2023-02-22 11:58:04 -0800

    use codegen

--- projects/capy/src/processor.ts
+++ projects/capy/src/processor.ts
@@ -1,67 +1,35 @@
-import { SuiBaseProcessor, SuiBindOptions, SuiNetwork } from '@sentio/sdk/sui'
+import { capy, eden, capy_market } from './types/sui/capy.js'
 
-class CapyProcessor extends SuiBaseProcessor {
-  static bind(options: SuiBindOptions): CapyProcessor {
-    return new CapyProcessor('capy', options)
+eden.bind()
+  .onEntryGetCapy((call, ctx) => {
+    ctx.meter.Counter('callEdenGetCapy').add(1)
   }
-}
+)
 
-CapyProcessor.bind({
-  address: '0x4c10b61966a34d3bb5c8a8f063e6b7445fc41f93',
-  network: SuiNetwork.TEST_NET,
-  startTimestamp: 1674831986485
-}).onMoveEvent(
-  (evt, ctx) => {
-    const gen = parseInt(evt.fields?.gen)
-    ctx.meter.Counter('evtCapyBorn').add(1)
-    ctx.meter.Counter('evtCapyBornTotalGens').add(gen)
-  },
-  {
-    type: 'capy::CapyBorn',
-  }
-).onMoveEvent(
-  (evt, ctx) => {
-    ctx.meter.Counter('evtItemAdded').add(1)
-  },
-  {
-    type: 'capy::ItemAdded',
-  }
-).onMoveEvent(
-  (evt, ctx) => {
-    ctx.meter.Counter('evtItemRemoved').add(1)
-  },
-  {
-    type: 'capy::ItemRemoved',
-  }
-).onMoveEvent(
-  (evt, ctx) => {
-    const price = parseInt(evt.fields?.price)
+capy_market.bind()
+  .onEventItemListed((evt, ctx) => {
+    const price = evt.fields_decoded.price
     ctx.meter.Counter('evtItemListed').add(1)
     ctx.meter.Counter('evtItemListedTotalPrice').add(price)
-  },
-  {
-    type: 'capy::ItemListed',
   }
-).onEntryFunctionCall(
-  (call, ctx) => {
-    var name = String(call.arguments?.at(2))
+)
+
+capy.bind()
+  .onEventCapyBorn((evt, ctx) => {
+    ctx.meter.Counter('evtCapyBorn').add(1)
+    ctx.meter.Counter('evtCapyBornTotalGens').add(evt.fields_decoded.gen)
+  }
+).onEventItemAdded((evt, ctx) => {
+    ctx.meter.Counter('evtItemAdded').add(1)
+  }
+).onEventItemRemoved((evt, ctx) => {
+    ctx.meter.Counter('evtItemRemoved').add(1)
+  }
+).onEntryAddGene((call, ctx) => {
+    var name = call.arguments_decoded[2]
     ctx.meter.Counter('callAdminAddGene').add(1, { "name": name })
-  }, 
-  {
-    function: 'capy_admin::add_gene',
   }
-).onEntryFunctionCall(
-  (call, ctx) => {
-    ctx.meter.Counter('callEdenGetCapy').add(1)
-  }, 
-  {
-    function: 'eden::get_capy',
-  }
-).onEntryFunctionCall(
-  (call, ctx) => {
+).onEntryBreedAndKeep((call, ctx) => {
     ctx.meter.Counter('callBreedAndKeep').add(1)
-  }, 
-  {
-    function: 'capy::breed_and_keep',
   }
 )
