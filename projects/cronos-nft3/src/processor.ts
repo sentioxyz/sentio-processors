646166d6230af1ae5711ce7b737279568623ce87
ye-sentio <ye@sentio.xyz>
2023-02-24 00:08:00 +0800

    update cronos lab

--- projects/cronos-nft3/src/processor.ts
+++ projects/cronos-nft3/src/processor.ts
@@ -2,7 +2,7 @@ import { MintedexchangeProcessor } from './types/eth/mintedexchange.js'
 
 
 //first tx block time 6006174	
-MintedexchangeProcessor.bind({ address: '0x40cBf9C75a46b147E0fd9aB47df5E064aE015f92', network: 25, startBlock: 7000000 })
+MintedexchangeProcessor.bind({ address: '0x40cBf9C75a46b147E0fd9aB47df5E064aE015f92', network: 25, startBlock: 6006174 })
   .onEventTakerBid(async (event, ctx) => {
     const taker = event.args.taker
     const maker = event.args.maker
@@ -26,18 +26,20 @@ MintedexchangeProcessor.bind({ address: '0x40cBf9C75a46b147E0fd9aB47df5E064aE015
     )
 
   })
-  .onEventRoyaltyPayment(async (event: any, ctx: any) => {
+  .onEventRoyaltyPayment(async (event, ctx) => {
     const collection = event.args.collection
     const tokenId = Number(event.args.tokenId)
     const royaltyRecipient = event.args.royaltyRecipient
     const currency = event.args.currency
     const amount = Number(event.args.amount) / Math.pow(10, 18)
 
+    console.log(collection, tokenId, royaltyRecipient, currency, amount)
+
     const hash = event.transactionHash
     try {
       // console.log("transactionHash", hash)
-      var tx = (await ctx.contract.provider.getTransaction(hash))!
-      var from = tx.from
+      const tx = (await ctx.contract.provider.getTransaction(hash))!
+      const from = tx.from
       console.log("event royaltypayment transactionHash", hash, "tx from:", from)
 
       ctx.eventLogger.emit("RoyaltyPayment", {
@@ -48,6 +50,7 @@ MintedexchangeProcessor.bind({ address: '0x40cBf9C75a46b147E0fd9aB47df5E064aE015
         currency: currency,
         amount: amount
       })
+
     } catch (e) {
       if (e instanceof Error) {
         console.log(e.message)
