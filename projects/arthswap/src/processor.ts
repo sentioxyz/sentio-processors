f948473b2fcdba7635cc73343f03dfaac0a28730
Kan Qiao <qiaokan.buaa@gmail.com>
2023-02-25 23:09:39 -0800

    hack to make arthswap run at least

--- projects/arthswap/src/processor.ts
+++ projects/arthswap/src/processor.ts
@@ -35,8 +35,15 @@ const FACTORY_ADDRESS = "0xA9473608514457b4bF083f9045fA63ae5810A03E"
 
 
 async function getTokenInfo(address: string): Promise<token.TokenInfo> {
+    // TODO(ye): this is wrong in the first place. this is a special address for native eth. does not apply here.
   if (address !== "0x0000000000000000000000000000000000000000") {
-    return await token.getERC20TokenInfo(address)
+      // This is a hack.
+    try {
+        return await token.getERC20TokenInfo(address)
+    } catch (error) {
+        console.log(error, address)
+        return token.NATIVE_ETH
+    }
   } else {
     return token.NATIVE_ETH
   }
@@ -54,8 +61,18 @@ let poolInfoMap = new Map<string, Promise<poolInfo>>()
 
 async function buildPoolInfo(token0Promise: Promise<string>,
   token1Promise: Promise<string>): Promise<poolInfo> {
-  const address0 = await token0Promise
-  const address1 = await token1Promise
+    let address0 =  ""
+    let address1 =  ""
+    try {
+        address0 = await token0Promise
+    } catch (error) {
+        console.log(error, address0)
+    }
+    try {
+        address1 = await token1Promise
+    } catch (error) {
+        console.log(error, address1)
+    }
   const tokenInfo0 = await getTokenInfo(address0)
   const tokenInfo1 = await getTokenInfo(address1)
   return {
