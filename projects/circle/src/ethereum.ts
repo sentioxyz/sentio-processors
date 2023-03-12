fd6fdb70ff8070247ca53e45c58982a833b2a8ce
Kan Qiao <qiaokan.buaa@gmail.com>
2023-03-12 15:57:55 -0700

    use resolution gauge for circle

--- projects/circle/src/ethereum.ts
+++ projects/circle/src/ethereum.ts
@@ -7,6 +7,7 @@ import { MintEvent, BurnEvent } from "./types/eth/fiattokenv2.js"
 import { FiatTokenV2Context, FiatTokenV2Processor } from "./types/eth/fiattokenv2.js"
 import { UChildAdministrableERC20Processor, UChildAdministrableERC20Context } from "./types/eth/uchildadministrableerc20.js"
 import { token } from "@sentio/sdk/utils"
+import {Gauge} from "@sentio/sdk";
 const DECIMAL = 6
 
 const totalSupplyHandler = async function(_:any, ctx: FiatTokenV2Context) {
@@ -15,17 +16,26 @@ const totalSupplyHandler = async function(_:any, ctx: FiatTokenV2Context) {
   ctx.meter.Gauge("total_supply").record(totalSupply, {labels: tokenInfo.symbol})
 }
 
+export const volOptions = {
+  sparse: true,
+  aggregationConfig: {
+    intervalInMinutes: [5],
+  }
+}
+const mint = Gauge.register("mint", volOptions)
+const burn = Gauge.register("burn", volOptions)
+
 const mintEventHandler = async function(event: MintEvent, ctx: FiatTokenV2Context) {
   const tokenInfo = await token.getERC20TokenInfo(ctx.contract.address)
   const amount = event.args.amount.scaleDown(DECIMAL)
-  ctx.meter.Gauge("mint").record(amount, {labels: tokenInfo.symbol})
+  mint.record(ctx, amount, {labels: tokenInfo.symbol})
   ctx.meter.Counter("mint_acc").add(amount, {labels: tokenInfo.symbol})
 }
 
 const burnEventHandler = async function(event: BurnEvent, ctx: FiatTokenV2Context) {
   const tokenInfo = await token.getERC20TokenInfo(ctx.contract.address)
   const amount = event.args.amount.scaleDown(DECIMAL)
-  ctx.meter.Gauge("burn").record(amount, {labels: tokenInfo.symbol})
+  burn.record(ctx, amount, {labels: tokenInfo.symbol})
   ctx.meter.Counter("burn_acc").add(amount, {labels: tokenInfo.symbol})
 }
 
