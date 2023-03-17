ba01e0912d710bac47793ee3229e2a1716dbee89
Kan Qiao <qiaokan.buaa@gmail.com>
2023-03-17 09:29:56 -0700

    use call trace for balancer

--- projects/balancer/src/processor.ts
+++ projects/balancer/src/processor.ts
@@ -3,14 +3,18 @@ import { ERC20Processor } from '@sentio/sdk/eth/builtin'
 import {VaultProcessor, VaultContext } from './types/eth/vault.js'
 import {MetaStablePoolProcessor, MetaStablePoolContext} from './types/eth/metastablepool.js'
 
-VaultProcessor.bind({address: "0xBA12222222228d8Ba445958a75a0704d566BF2C8"}).
-onEventSwap(async (event, ctx) => {
-}).onEventPoolRegistered(async (event, ctx) => {
-    console.log(event.args.poolAddress, event.args.poolId)
+VaultProcessor.bind({address: "0xBA12222222228d8Ba445958a75a0704d566BF2C8"})
+    .onCallSwap(async (call, tx) => {
+        if (call.error) {
+            return
+        }
+    console.log(call.args.funds.sender, call.args.funds.recipient,
+        call.args.singleSwap.amount, call.args.singleSwap.assetIn)
 })
 
+/*
 MetaStablePoolProcessor.bind({address: "0x32296969Ef14EB0c6d29669C550D4a0449130230"})
-    .onEventTransfer(async (evt, ctx) =>{
+.onEventTransfer(async (evt, ctx) =>{
         console.log(evt.args.from, evt.args.to)
 })
-
+*/
