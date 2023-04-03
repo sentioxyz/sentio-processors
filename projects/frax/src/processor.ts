99308f363231e19df08fadc0dad18d5d2a0d57e0
Kan Qiao <qiaokan.buaa@gmail.com>
2023-04-03 01:03:38 -0700

    add try catch

--- projects/frax/src/processor.ts
+++ projects/frax/src/processor.ts
@@ -24,10 +24,14 @@ FRAXStablecoinProcessor.bind({address: "0x853d955acef822db058eb8505911ed77f175b9
     vol.record(ctx, evt.args.value.scaleDown(18))
 })
 .onTimeInterval(async (_ : any, ctx)=>{
-    const supply = await ctx.contract.totalSupply()
-    ctx.meter.Gauge("frax_supply").record(supply.scaleDown(18))
-    const col = await ctx.contract.global_collateral_ratio()
-    ctx.meter.Gauge("global_collateral_ratio").record(col.scaleDown(18))
-    const price = await ctx.contract.frax_price()
-    ctx.meter.Gauge("frax_price").record(price.scaleDown(6))
+    try {
+        const supply = await ctx.contract.totalSupply()
+        ctx.meter.Gauge("frax_supply").record(supply.scaleDown(18))
+        const col = await ctx.contract.global_collateral_ratio()
+        ctx.meter.Gauge("global_collateral_ratio").record(col.scaleDown(18))
+        const price = await ctx.contract.frax_price()
+        ctx.meter.Gauge("frax_price").record(price.scaleDown(6))
+    } catch (e) {
+        console.log(e)
+    }
 })
