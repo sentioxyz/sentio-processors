0eba617a292d53d5d57d734b23f9d4a5032a707f
Fuyao Zhao <zfy0701@gmail.com>
2023-04-06 22:05:35 -0700

    fix sui demo

--- projects/sui-demo/src/processor.ts
+++ projects/sui-demo/src/processor.ts
@@ -8,6 +8,11 @@
 import { sui_system, validator } from '@sentio/sdk/sui/builtin/0x3'
 import { SuiNetwork, SuiDynamicFieldObjectsProcessor } from '@sentio/sdk/sui'
 import RequestAddStakePayload = sui_system.RequestAddStakePayload
+import { dynamic_field } from "@sentio/sdk/sui/builtin/0x2";
+import {
+  single_collateral
+} from "./types/sui/testnet/0xebaa2ad3eacc230f309cd933958cc52684df0a41ae7ac214d186b80f830867d2.js";
+import { sui } from "./types/sui/testnet/0xd175cff04f1d49574efb6f138bc3b9b7313915a57b5ca04141fb1cb4f66984b2.js";
 
 validator.bind({ network: SuiNetwork.TEST_NET }).onEventStakingRequestEvent((evt, ctx) => {
   const amount_original = BigInt(evt.parsedJson?.amount)
@@ -23,10 +28,10 @@ sui_system.bind({ network: SuiNetwork.TEST_NET }).onEntryRequestAddStake((call: 
 SuiDynamicFieldObjectsProcessor.bind({
   network: SuiNetwork.TEST_NET,
   objectId: '0xdcb1f0c4d31528a67f89303e3a99e15b9e21c7e22b4123a0e43e90b3fae5ea1e',
-}).onTimeInterval((fields, ctx) => {
+}).onTimeInterval((fields: dynamic_field.Field<bigint, single_collateral.PortfolioVault<sui.SUI, sui.SUI, sui.SUI>>[], ctx) => {
   ctx.meter.Gauge('num_portfolio_vault').record(fields.length)
   fields.forEach((vault) => {
     ctx.meter.Gauge('deposit_vault_active_sub_vault_balance').record(
-      vault.value.deposit_vault?.active_sub_vault?.balance, { name: vault.name })
+      vault.value.deposit_vault?.active_sub_vault?.balance.value, { name: vault.name.toString() })
   });
 })
