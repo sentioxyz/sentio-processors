3e8723e94f12d1c3e429260b893288c1c9f79c13
Kan Qiao <qiaokan.buaa@gmail.com>
2023-04-08 17:33:28 -0700

    track different implementations for paraswap

--- projects/paraswap/src/processor.ts
+++ projects/paraswap/src/processor.ts
@@ -1,6 +1,7 @@
 import { Counter, Gauge } from '@sentio/sdk'
 import { ERC20Processor } from '@sentio/sdk/eth/builtin'
 import { MultiPathProcessor, MultiPathContext } from './types/eth/multipath.js'
+import { AugustusSwapperProcessor, AugustusSwapperContext } from './types/eth/augustusswapper.js'
 
 MultiPathProcessor.bind({address: "0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57"})
 .onEventSwappedV3(async (evt, ctx) => {
@@ -8,4 +9,14 @@ MultiPathProcessor.bind({address: "0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57"})
 })
 .onEventBoughtV3(async (evt, ctx) => {
     ctx.meter.Counter("buys").add(1)
+})
+
+AugustusSwapperProcessor.bind({address: "0xDEF171Fe48CF0115B1d80b88dc8eAB59176FEe57"})
+.onCallSetImplementation(async (call, ctx) => {
+    if (call.error) {
+        ctx.meter.Counter("setImplementationErrors").add(1)
+        return
+    }
+    console.log("setImplementation", call.args.selector, call.args.implementation.toString())
+    ctx.meter.Counter("setImplementations").add(1)
 })
