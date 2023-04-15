80c1ca444b604621cf1e566ca096376ee4c4e29c
ye-sentio <ye@sentio.xyz>
2023-04-15 22:25:34 +0800

    argo test eventlog & view function

--- projects/argo/src/processor.ts
+++ projects/argo/src/processor.ts
@@ -1,22 +1,51 @@
 import { Counter, Gauge } from '@sentio/sdk'
 import { ERC20Processor } from '@sentio/sdk/eth/builtin'
 import { AtlantisRacingProcessor } from './types/eth/atlantisracing.js'
-
+import { AtlantisGemstonesProcessor } from './types/eth/atlantisgemstones.js'
 export const ATLANTIS_SPACESHIP_RACING_ADDRESS = "0x9b1a00168aea4d6e0f223425a53b5df7195b44ef"
+export const ATLANTIS_GEMSTONES_ADDRESS = "0x6933bd3c56d3fc282bc0c1931f613f79bd9430b4"
 
-//testnet
+//cronos testnet chain_id = 338
 AtlantisRacingProcessor.bind({ address: ATLANTIS_SPACESHIP_RACING_ADDRESS, network: 338 })
-  .onEventPayout(async (event, ctx) => {
-    const poolId = Number(event.args.poolId)
+  // .onEventPayout(async (event, ctx) => {
+  //   const poolId = Number(event.args.poolId)
+  //   const user = event.args.user
+  //   const goldAmount = Number(event.args.goldAmount)
+  //   const stardustAmount = Number(event.args.stardustAmount)
+  //   ctx.eventLogger.emit("Payout",
+  //     {
+  //       distinctId: user,
+  //       poolId,
+  //       goldAmount,
+  //       stardustAmount
+  //     })
+  // })
+  .onEventStake(async (event, ctx) => {
+    const tokenID = Number(event.args.tokenID)
     const user = event.args.user
-    const goldAmount = Number(event.args.goldAmount)
-    const stardustAmount = Number(event.args.stardustAmount)
-    ctx.eventLogger.emit("Payout",
+    const shipScore = Number(event.args.shipScore)
+    ctx.eventLogger.emit("Stake",
       {
         distinctId: user,
-        poolId,
-        goldAmount,
-        stardustAmount
+        tokenID,
+        shipScore
       })
   })
+
+
+AtlantisGemstonesProcessor.bind({ address: ATLANTIS_GEMSTONES_ADDRESS, network: 338 })
+  .onAllEvents(async (event, ctx) => {
+    ctx.meter.Counter("all_event_counter").add(1)
+    let gemSupply = 0
+    for (let i = 1; i < 13; i++) {
+      try {
+        gemSupply = Number(await ctx.contract.totalSupply(i))
+        ctx.meter.Gauge(`gemSupply`).record(gemSupply, { "ID": i.toString() })
+      }
+      catch (e) {
+        console.log(e.message, `Get totalSupply(${i}) error at ${event.transactionHash}`)
+      }
+    }
+  })
+
 
