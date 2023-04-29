c873677a849b6a44ccce6ca7f2f6a5494bc76882
Kan Qiao <qiaokan.buaa@gmail.com>
2023-04-28 19:41:32 -0700

    add a few cases to solve for mev analytics

--- projects/pendle-public-dashboard-debug/src/processor.ts
+++ projects/pendle-public-dashboard-debug/src/processor.ts
@@ -1,17 +1,17 @@
-import ethConfig from './config/main.json';
-import arbConfig from './config/arbitrum-one.json';
-import { Counter, Gauge } from '@sentio/sdk';
-import { ERC20Processor } from '@sentio/sdk/eth/builtin';
-import { PendleMarketFactoryProcessor } from './types/eth/pendlemarketfactory.js';
-import { tradingVolume_handleCreateNewMarket } from './processes/trading-volume.js';
+import ethConfig from "./config/main.json";
+import arbConfig from "./config/arbitrum-one.json";
+import { Counter, Gauge } from "@sentio/sdk";
+import { ERC20Processor } from "@sentio/sdk/eth/builtin";
+import { PendleMarketFactoryProcessor } from "./types/eth/pendlemarketfactory.js";
+import { tradingVolume_handleCreateNewMarket } from "./processes/trading-volume.js";
 
 PendleMarketFactoryProcessor.bind({
   address: ethConfig.PendleMarketFactory,
   startBlock: ethConfig.PendleMarketFactory_startBlock,
 }).onEventCreateNewMarket(tradingVolume_handleCreateNewMarket);
 
-// PendleMarketFactoryProcessor.bind({
-//   address: arbConfig.PendleMarketFactory,
-//   startBlock: arbConfig.PendleMarketFactory_startBlock,
-//   network: 42161,
-// }).onEventCreateNewMarket(tradingVolume_handleCreateNewMarket);
+PendleMarketFactoryProcessor.bind({
+  address: arbConfig.PendleMarketFactory,
+  startBlock: arbConfig.PendleMarketFactory_startBlock,
+  network: 42161,
+}).onEventCreateNewMarket(tradingVolume_handleCreateNewMarket);
