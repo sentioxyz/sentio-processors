f41677af5fc33275d92914fc77293ac140f0a042
Fuyao Zhao <zfy0701@gmail.com>
2023-06-17 18:47:13 +0800

    update sdk, add test sui global processor

--- projects/global_test/src/processor.test.ts
+++ projects/global_test/src/processor.test.ts
@@ -1,7 +1,12 @@
 import { TestProcessorServer } from '@sentio/sdk/testing'
+import { decodeMultiSig } from "@mysten/sui.js";
+import { TextDecoder, TextEncoder } from "util"
 
 describe('Test Processor', () => {
   const service = new TestProcessorServer(() => import('./processor.js'))
+
+  // const txSig = 'AwUAG3RvCWmw0F7VmCsDgVMYUCcggRmh8D5lyJo2BKU+LFJgfbHi4eIY+PmjmX1yaOZTemUJ8ENrCcqbZ0moqgSGDQCp1dK8FW7pSn7ZW8l0F8O8QCI3xbIrD3PNu6XHNd2GXjfO60ndCCOAillFoDJGRzA7QLD4CfQhJfRuMbbUwq0DACRG8vZPGMIQqRDaKv2uCyLNGamGUh3/5pLarLHrVeHdrx21Fq4Y/GQsPTJ0mAhu1E3IeFzWXG9nKWSzU8YU6QwACQD4umqxoOjBVNDEF9CVjYqMGItKkxvavtn3fcTLCPZ79fUHB3IpRAp3HD7PcORyDbQzFOXAr3KZO79hNT0fAwAuE+CvzKiCVZhxWOBucKUI6EEF6FM2NWAC17Ob8iWGMjDoaytdmX06aBdjoaLnUKvT8sPdH7zUQvckNepBoOwBGjowAAABAAAAAAAEABAAAAAAAAEAAgADAAQABSxBRVN4Y3pxKzlKbFBodnZVVVB4NTk4R2tubGtQSDYxSW41cGJQeklwU29RTgEsQU1CdEg0QTdyd0pwejUwRVZXSk1MbENpNk5nRUNBNDdwS0k5amUzN0ZmcWgBLEFFUDhWU0JWeUloR01Pa0tWUUIyUnpGczgxUTdOeWQ3ZXdtZTZsTzBJSFppASxBRWJrNS9XY2tEeno2ZTJoZHF2Wll4YjUxVXk5K3VvNStKSmZ2WTgwaVNLMgEsQUhLTEZhMnphKzFDQUgyNXdhbTBGNkNIaHdwQnhyNFcwWWZwQVBvdldWTEoBAwA='
+
 
   beforeAll(async () => {
     await service.start()
@@ -10,5 +15,17 @@ describe('Test Processor', () => {
   test('has config', async () => {
     const config = await service.getConfig({})
     expect(config.contractConfigs.length > 0)
+  })
+
+  const txSig = 'AwUAG3RvCWmw0F7VmCsDgVMYUCcggRmh8D5lyJo2BKU+LFJgfbHi4eIY+PmjmX1yaOZTemUJ8ENrCcqbZ0moqgSGDQCp1dK8FW7pSn7ZW8l0F8O8QCI3xbIrD3PNu6XHNd2GXjfO60ndCCOAillFoDJGRzA7QLD4CfQhJfRuMbbUwq0DACRG8vZPGMIQqRDaKv2uCyLNGamGUh3/5pLarLHrVeHdrx21Fq4Y/GQsPTJ0mAhu1E3IeFzWXG9nKWSzU8YU6QwACQD4umqxoOjBVNDEF9CVjYqMGItKkxvavtn3fcTLCPZ79fUHB3IpRAp3HD7PcORyDbQzFOXAr3KZO79hNT0fAwAuE+CvzKiCVZhxWOBucKUI6EEF6FM2NWAC17Ob8iWGMjDoaytdmX06aBdjoaLnUKvT8sPdH7zUQvckNepBoOwBGjowAAABAAAAAAAEABAAAAAAAAEAAgADAAQABSxBRVN4Y3pxKzlKbFBodnZVVVB4NTk4R2tubGtQSDYxSW41cGJQeklwU29RTgEsQU1CdEg0QTdyd0pwejUwRVZXSk1MbENpNk5nRUNBNDdwS0k5amUzN0ZmcWgBLEFFUDhWU0JWeUloR01Pa0tWUUIyUnpGczgxUTdOeWQ3ZXdtZTZsTzBJSFppASxBRWJrNS9XY2tEeno2ZTJoZHF2Wll4YjUxVXk5K3VvNStKSmZ2WTgwaVNLMgEsQUhLTEZhMnphKzFDQUgyNXdhbTBGNkNIaHdwQnhyNFcwWWZwQVBvdldWTEoBAwA='
+
+  const tx2 = 'AwIAvlJnUP0iJFZL+QTxkKC9FHZGwCa5I4TITHS/QDQ12q1sYW6SMt2Yp3PSNzsAay0Fp2MPVohqyyA02UtdQ2RNAQGH0eLk4ifl9h1I8Uc+4QlRYfJC21dUbP8aFaaRqiM/f32TKKg/4PSsGf9lFTGwKsHJYIMkDoqKwI8Xqr+3apQzAwADAFriILSy9l6XfBLt5hV5/1FwtsIsAGFow3tefGGvAYCDAQECHRUjB8a3Kw7QQYsOcM2A5/UpW42G9XItP1IT+9I5TzYCADtqJ7zOtqQtYqOo0CpvDXNlMhV3HeJDpjrASKGLWdopAwMA'
+
+  test('test multisign', async () => {
+    const keys = decodeMultiSig(tx2)
+    const key = keys[0].pubKey.toBytes()
+    // const new TextDecoder().decode(key)
+    const buf = Buffer.from(key).toString("hex")
+    console.log(buf)
   })
 })
--- projects/global_test/src/processor.ts
+++ projects/global_test/src/processor.ts
@@ -1,5 +1,8 @@
 import * as liquidswap from "./types/aptos/liquidswap.js";
 import { AptosBaseProcessor, AptosModulesProcessor } from "@sentio/sdk/aptos";
+import { SuiGlobalProcessor, SuiNetwork } from "@sentio/sdk/sui";
+import { decodeMultiSig } from "@mysten/sui.js"
+// import { SuiModulesProcessor } from "@sentio/sdk/sui";
 
 for (const s of [liquidswap.scripts_v3, liquidswap.scripts_v2, liquidswap.scripts,
   liquidswap.liquidity_pool, liquidswap.dao_storage, liquidswap.global_config, liquidswap.lp_account]) {
@@ -18,3 +21,25 @@ new AptosBaseProcessor("", { address })
 // TODO make this work, the number should be larger than "manual" for a bit
 AptosModulesProcessor.bind({address})
     .onTransaction((tx, ctx) => { ctx.meter.Counter("auto_2").add(1) })
+
+// SuiModulesProcessor.bind({ address: '0x1be2df58d54d20d336886ef2c34d11c1d3ba194d53beb955318b8f6350acdb86', startCheckpoint: 1603788n })
+//     .onTransactionBlock((tx, ctx) => {
+//       ctx.eventLogger("transaction", {})
+//     })
+
+SuiGlobalProcessor.bind({ network: SuiNetwork.MAIN_NET, startCheckpoint: 1603788n })
+  .onTransactionBlock((tx, ctx) => {
+    const txSig = tx.transaction?.txSignatures[0]
+    if (txSig) {
+      const sigs = decodeMultiSig(txSig)
+      ctx.eventLogger.emit("multisig", {
+        message: txSig
+      })
+
+    } else {
+      console.log("No sig found")
+    }
+  },
+  {
+    publicKeyPrefix: "0x5ae220b4b2f65e977c12ede61579ff5170b6c22c006168c37b5e7c61af018083"
+  })
