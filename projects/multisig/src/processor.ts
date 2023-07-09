f59bc808a2e4f0f5f738ca878a24bb4942e276c2
Fuyao Zhao <zfy0701@gmail.com>
2023-07-09 21:28:38 +0800

    multisig test

---
+++ projects/multisig/src/processor.ts
@@ -0,0 +1,26 @@
+import { SuiGlobalProcessor, SuiNetwork } from '@sentio/sdk/sui';
+import { Exporter } from '@sentio/sdk';
+
+const MAVEN_PREFIX = '0x6d6176656e';
+const ExecuteTransaction = Exporter.register('ExecuteTransaction', 'ExecuteTransaction');
+
+SuiGlobalProcessor.bind({ network: SuiNetwork.TEST_NET, startCheckpoint: 4928686n }).onTransactionBlock(
+    (tx, ctx) => {
+      const txSig = tx.transaction?.txSignatures[0];
+      if (txSig) {
+        ExecuteTransaction.emit(ctx, {
+          signature: txSig,
+          payloadHash: tx.transaction.payloadHash,
+        });
+        ctx.eventLogger.emit('multisigTx', {
+          signature: txSig,
+          payloadHash: tx.transaction.payloadHash,
+          message: `received a multisig transaction`,
+        });
+      } else {
+        console.log('No sig found');
+      }
+    },
+    {
+      publicKeyPrefix: MAVEN_PREFIX,
+    })
