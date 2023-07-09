70390b90c034614df1d1ab23d91fc034779db12f
Fuyao Zhao <zfy0701@gmail.com>
2023-07-09 13:34:16 +0800

    forked pool project

---
+++ projects/forked-pools/src/processor.ts
@@ -0,0 +1,29 @@
+import { UniswapV2FactoryProcessor , UniswapV3FactoryProcessor} from "./types/eth/index.js";
+
+UniswapV2FactoryProcessor.bind({address: '*', startBlock: 10000835})
+    .onEventPairCreated((evt, ctx) => {
+
+      ctx.meter.Counter("pool").add(1, {
+        poolType: "UniswapV2",
+      })
+
+      ctx.eventLogger.emit("PoolCreated", {
+        poolType: "UniswapV2",
+        address: evt.args.pair
+      })
+    })
+
+UniswapV3FactoryProcessor.bind({address: '*', startBlock: 12369621})
+    .onEventPoolCreated((evt, ctx) => {
+      ctx.meter.Counter("pool").add(1, {
+        poolType: "UniswapV2",
+      })
+
+      ctx.eventLogger.emit("PoolCreated", {
+        poolType: "UniswapV2",
+        address: evt.args.pool
+      })
+    })
+
+//
+
