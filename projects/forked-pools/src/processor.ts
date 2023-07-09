7ecdd6747fb2d20ccb947c4470d87c4baff293e2
Kan Qiao <qiaokan.buaa@gmail.com>
2023-07-09 10:57:02 -0700

    filter out original v2/v3

--- projects/forked-pools/src/processor.ts
+++ projects/forked-pools/src/processor.ts
@@ -1,33 +1,36 @@
-import { UniswapV2FactoryProcessor , UniswapV3FactoryProcessor} from "./types/eth/index.js";
+import {
+  UniswapV2FactoryProcessor,
+  UniswapV3FactoryProcessor,
+} from "./types/eth/index.js";
 
-UniswapV2FactoryProcessor.bind({address: '*', startBlock: 10000835})
-    .onEventPairCreated((evt, ctx) => {
+UniswapV2FactoryProcessor.bind({
+  address: "*",
+  startBlock: 10000835,
+}).onEventPairCreated((evt, ctx) => {
+  if (
+    evt.address.toLowerCase() ===
+    "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f".toLowerCase()
+  ) {
+    return;
+  }
+  ctx.meter.Counter("pool").add(1, {
+    poolType: "UniswapV2",
+    factory: evt.address,
+  });
+});
 
-      ctx.meter.Counter("pool").add(1, {
-        poolType: "UniswapV2",
-      })
-
-      ctx.eventLogger.emit("PoolCreated", {
-        poolType: "UniswapV2",
-        poolAddress: evt.args.pair,
-        token0: evt.args.token0,
-        token1: evt.args.token1,
-      })
-    })
-
-UniswapV3FactoryProcessor.bind({address: '*', startBlock: 12369621})
-    .onEventPoolCreated((evt, ctx) => {
-      ctx.meter.Counter("pool").add(1, {
-        poolType: "UniswapV3",
-      })
-
-      ctx.eventLogger.emit("PoolCreated", {
-        poolType: "UniswapV3",
-        poolAddress: evt.args.pool,
-        token0: evt.args.token0,
-        token1: evt.args.token1,
-      })
-    })
-
-//
-
+UniswapV3FactoryProcessor.bind({
+  address: "*",
+  startBlock: 12369621,
+}).onEventPoolCreated((evt, ctx) => {
+  if (
+    evt.address.toLowerCase() ===
+    "0x1F98431c8aD98523631AE4a59f267346ea31F984".toLowerCase()
+  ) {
+    return;
+  }
+  ctx.meter.Counter("pool").add(1, {
+    poolType: "UniswapV3",
+    factory: evt.address,
+  });
+});
