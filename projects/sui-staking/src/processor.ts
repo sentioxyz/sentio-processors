c1d09bf48c804d51655c073ec3b9439a6937165f
ye <ye@sentio.xyz>
2023-08-17 20:55:19 +0800

    added validator info event and join event

--- projects/sui-staking/src/processor.ts
+++ projects/sui-staking/src/processor.ts
@@ -1,6 +1,7 @@
 import { validator } from "@sentio/sdk/sui/builtin/0x3"
 import { SuiNetwork } from "@sentio/sdk/sui"
 export const VALIDATOR_ADDRESS = "0x3"
+import { validator_set } from "@sentio/sdk/sui/builtin/0x3"
 
 validator.bind({
   address: VALIDATOR_ADDRESS,
@@ -48,5 +49,41 @@ validator.bind({
       unstaking_epoch,
       principal_amount,
       reward_amount
+    })
+  })
+
+
+validator_set.bind()
+  .onEventValidatorEpochInfoEventV2(async (event, ctx) => {
+    ctx.eventLogger.emit("ValidatorEpochInfoEventV2", {
+      distinctId: event.data_decoded.validator_address,
+      epoch: event.data_decoded.epoch,
+      validator_address: event.data_decoded.validator_address,
+      reference_gas_survey_quote: event.data_decoded.reference_gas_survey_quote,
+      stake: Number(event.data_decoded.stake) / Math.pow(10, 9),
+      voting_power: event.data_decoded.voting_power,
+      commission_rate: event.data_decoded.commission_rate,
+      pool_staking_reward: Number(event.data_decoded.pool_staking_reward) / Math.pow(10, 9),
+      storage_fund_staking_reward: Number(event.data_decoded.storage_fund_staking_reward) / Math.pow(10, 9),
+      pool_token_exchange_rate: event.data_decoded.pool_token_exchange_rate,
+      // tallying_rule_reporters: event.data_decoded.,
+      tallying_rule_global_score: event.data_decoded.tallying_rule_global_score
+    })
+  })
+  .onEventValidatorJoinEvent(async (event, ctx) => {
+    ctx.eventLogger.emit("ValidatorJoinEvent", {
+      distinctId: event.data_decoded.validator_address,
+      validator_address: event.data_decoded.validator_address,
+      staking_pool_id: event.data_decoded.staking_pool_id,
+      epoch: event.data_decoded.epoch
+    })
+  })
+  .onEventValidatorLeaveEvent(async (event, ctx) => {
+    ctx.eventLogger.emit("ValidatorJoinEvent", {
+      distinctId: event.data_decoded.validator_address,
+      validator_address: event.data_decoded.validator_address,
+      staking_pool_id: event.data_decoded.staking_pool_id,
+      epoch: event.data_decoded.epoch,
+      is_voluntary: event.data_decoded.is_voluntary
     })
   })
