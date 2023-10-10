3120ddb4a8e7fa9110487c573510155dac6df921
Kan Qiao <qiaokan.buaa@gmail.com>
2023-10-10 01:06:04 -0700

    add pancake v3

--- projects/bsc-pancake/src/processor.ts
+++ projects/bsc-pancake/src/processor.ts
@@ -1,15 +1,16 @@
 import { Counter, Gauge } from "@sentio/sdk";
 import { ERC20Processor } from "@sentio/sdk/eth/builtin";
 import { PancakePairProcessor } from "./types/eth/pancakepair.js";
+import { PancakeV3PoolProcessor } from "./types/eth/pancakev3pool.js";
 import { EthChainId } from "@sentio/sdk/eth";
+
+const START_BLOCK = 32473281;
 
 const V2_ADDRESSES = [
   "0x4c85aace702587c977e1372d44a12addfb6f3402",
   "0xdac44d168c3c050c47b0d9bb067aabfd7497751f",
   "0x4e7ae8129311f3670964cd58da28de56d4eeff41",
 ];
-
-const START_BLOCK = 32446499;
 
 for (const address of V2_ADDRESSES) {
   PancakePairProcessor.bind({
@@ -18,6 +19,24 @@ for (const address of V2_ADDRESSES) {
     startBlock: START_BLOCK,
   }).onEventSwap(async (evt, ctx) => {
     ctx.eventLogger.emit("V2SwapEvent", {
+      sender: evt.args.sender,
+      index: evt.transactionIndex,
+    });
+  });
+}
+
+const V3_ADDRESSES = [
+  "0x0f338Ec12d3f7C3D77A4B9fcC1f95F3FB6AD0EA6",
+  "0x7d05c84581f0C41AD80ddf677A510360bae09a5A",
+];
+
+for (const address of V3_ADDRESSES) {
+  PancakeV3PoolProcessor.bind({
+    address: address,
+    network: EthChainId.BINANCE,
+    startBlock: START_BLOCK,
+  }).onEventSwap(async (evt, ctx) => {
+    ctx.eventLogger.emit("V3SwapEvent", {
       sender: evt.args.sender,
       index: evt.transactionIndex,
     });
