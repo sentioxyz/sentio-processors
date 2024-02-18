b08940850ee2352c849d973a2cbbb47e8ee9a586
Kan Qiao <qiaokan.buaa@gmail.com>
2024-02-18 08:44:35 -0800

    add blacklist

--- projects/eth-mev-analytics/src/common.ts
+++ projects/eth-mev-analytics/src/common.ts
@@ -72,6 +72,8 @@ export const chainConfigs = [
       "0x10eBee1d8d831B21ED40098274633367d0F52516".toLowerCase(), // safe
       "0x080AC7ec9995da5Cfdda4A14058ce0c872B22087".toLowerCase(), // some strategy
       "0x227bABe533fa9a1085f5261210E0B7137E44437B".toLowerCase(), // some strategy
+      "0x896D9b9Eee18F6C88C5575B78247834029375575".toLowerCase(), // liquidity hub
+      "0xA45DE4Bc6bBe70B5dB9AFaD5bbB125319e78AA9b".toLowerCase(), // some strategy
     ]),
     mintBurnAddr: new Set<string>([
       "0x0000000000000000000000000000000000000000",
