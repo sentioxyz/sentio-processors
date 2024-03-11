055bb1d97ac8577f23d0260125bffef6b0094975
Fuyao Zhao <zfy0701@gmail.com>
2024-03-11 15:55:29 -0700

    add missing file

---
+++ projects/sui-nft/src/localdb.ts
@@ -0,0 +1,20 @@
+import { AsyncNedb } from "nedb-async";
+
+export const db = new AsyncNedb({
+  filename: "/data/nft.db",
+  autoload: true,
+});
+
+type NFTListing = {
+  _id: string;
+  seller: string;
+};
+
+export function setSeller(id: string, seller: string) {
+  return db.asyncUpdate({ _id: id }, { _id: id, seller }, { upsert: true });
+}
+
+export async function getSeller(id: string) {
+  const listing = await db.asyncFindOne<NFTListing>({ _id: id });
+  return listing.seller
+}
