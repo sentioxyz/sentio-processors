142202483ea733543bff384881572e2f783702f9
Fuyao Zhao <zfy0701@gmail.com>
2024-05-13 02:27:40 -0700

    adjust spark

--- projects/spark/src/processor.ts
+++ projects/spark/src/processor.ts
@@ -1,16 +1,31 @@
 import { OrderbookProcessor } from "./types/fuel/OrderbookProcessor.js";
 import { FuelNetwork } from "@sentio/sdk/fuel";
-import { GLOBAL_CONFIG } from '@sentio/runtime';
-
-GLOBAL_CONFIG.execution = {
-  sequential: true,
-};
 
 OrderbookProcessor.bind({
   chainId: FuelNetwork.TEST_NET,
-  address: '0x7134802bdefd097f1c9d8ad86ef27081ae609b84de0afc87b58bd4e04afc6a23'
-}).onCallMatch_orders(async (order, ctx) => {
-  const sellId = order.args[0]
-  const buyId = order.args[1]
-
-}, {})
+  // startBlock: 10977001n,
+  // address: '0x7134802bdefd097f1c9d8ad86ef27081ae609b84de0afc87b58bd4e04afc6a23'
+  address: '0x0f0c1065a7b82d026069c5cf070b21ee65713fd1ac92ec1d25eacc3100187f78'
+})
+//     .onCallMatch_orders(async (order, ctx) => {
+//   const trades = order.getLogsOfTypeTradeEvent()
+//   console.log("decode trades: ", trades.length)
+//   for (const trade of trades) {
+//     ctx.eventLogger.emit('trade', {
+//       ...trade
+//     })
+//   }
+// })
+    .onLogTradeEvent(async (trade, ctx) => {
+        const pool = trade.data.base_token.value
+        ctx.eventLogger.emit('trade', {
+          distinctId: ctx.transaction?.sender,
+          ...trade
+        })
+    })
+    .onLogMarketCreateEvent(async (order, ctx) => {
+        ctx.eventLogger.emit('order', {
+          distinctId: ctx.transaction?.sender,
+          ...order
+        })
+    })
