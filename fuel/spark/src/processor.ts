cfeea3c7da83d67747960b616efc0c6b76ab8866
Fuyao Zhao <zfy0701@gmail.com>
2024-05-19 20:07:18 -0700

    upgrade and update syntax

--- fuel/spark/src/processor.ts
+++ fuel/spark/src/processor.ts
@@ -1,27 +1,17 @@
 import { OrderbookProcessor } from "./types/fuel/OrderbookProcessor.js";
 import { FuelNetwork } from "@sentio/sdk/fuel";
+import { BigDecimal } from "@sentio/sdk";
 
 OrderbookProcessor.bind({
   chainId: FuelNetwork.TEST_NET,
-  // startBlock: 10977001n,
-  // address: '0x7134802bdefd097f1c9d8ad86ef27081ae609b84de0afc87b58bd4e04afc6a23'
   address: '0x0f0c1065a7b82d026069c5cf070b21ee65713fd1ac92ec1d25eacc3100187f78'
 })
-//     .onCallMatch_orders(async (order, ctx) => {
-//   const trades = order.getLogsOfTypeTradeEvent()
-//   console.log("decode trades: ", trades.length)
-//   for (const trade of trades) {
-//     ctx.eventLogger.emit('trade', {
-//       ...trade
-//     })
-//   }
-// })
     .onLogTradeEvent(async (trade, ctx) => {
-        const vol = BigInt(trade.data.trade_price.toString()) * BigInt(trade.data.trade_size.toString())
+        const vol = trade.data.trade_price.mul(trade.data.trade_size).scaleDown(2 * 10)
         ctx.eventLogger.emit('trade', {
           distinctId: ctx.transaction?.sender,
           ...trade,
-          vol: vol.scaleDown(2 * 10)
+          vol: vol
         })
     })
     .onLogOrderChangeEvent(async (order, ctx) => {
