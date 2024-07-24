8bff9cb3756981a23976498cfe27fed901813b25
lwedge99 <116623885+lwedge99@users.noreply.github.com>
2024-07-24 19:11:32 +0800

    add inception gamma

---
+++ points/inception/linea-gamma-points/src/config.ts
@@ -0,0 +1,6 @@
+import { EthChainId } from "@sentio/sdk/eth";
+
+export const TOKEN_DECIMALS = 18;
+
+export const NETWORK = EthChainId.LINEA;
+export const HYPERVISOR_ADDRESS = "0xc491c1b173e932e97d9f739ccd9ae5b6d5fce4ce";
