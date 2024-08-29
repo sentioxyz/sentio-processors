fbf1818e8266d09cc727d2af6218232a66c76ddd
Ping Chen <remotenonsense@gmail.com>
2024-08-29 22:58:46 +0900

    layerbank: use exchangeRate when computing supply

--- projects/layerbank/src/processor.ts
+++ projects/layerbank/src/processor.ts
@@ -3,29 +3,35 @@ import { EthChainId } from '@sentio/sdk/eth'
 import { token as tokens } from '@sentio/sdk/utils'
 import { getLayerbankContract, LayerbankContext, LayerbankProcessor } from './types/eth/layerbank.js'
 import { getLtokenContract, getLtokenContractOnContext, LtokenContext, LtokenProcessor } from './types/eth/ltoken.js'
+import { UserMarkets } from './schema/schema.js'
 
 const address = '0xEC53c830f4444a8A56455c6836b5D2aA794289Aa'
 const startBlock = 152083
-// const startBlock = 8144000
+// const startBlock = 8810000
 
-const tokenInfos = new Map<string, tokens.TokenInfo>()
+const tokenInfos = new Map<string, { token: string; tokenInfo: tokens.TokenInfo }>()
 
-async function recordBalance(ctx: LtokenContext, lToken: string, user: string, type: string) {
-  const tokenInfo = tokenInfos.get(lToken)
+async function recordBalance(ctx: LayerbankContext | LtokenContext, lToken: string, user: string, event = 'snapshot') {
+  const { token, tokenInfo } = tokenInfos.get(lToken) || {}
   if (!tokenInfo) {
     console.warn('no token info', ctx.transactionHash, lToken)
     return
   }
   const contract = getLtokenContractOnContext(ctx, lToken)
-  const [supply, borrow] = await Promise.all([contract.balanceOf(user), contract.borrowBalanceOf(user)])
+  const { lTokenBalance, borrowBalance, exchangeRate } = await contract.accountSnapshot(user)
+  const rate = scaleDown(exchangeRate, tokenInfo.decimal)
 
-  ctx.eventLogger.emit('balance', {
-    lToken,
-    user,
-    symbol: tokenInfo.symbol,
-    supply: scaleDown(supply, tokenInfo.decimal),
-    borrow: scaleDown(borrow, tokenInfo.decimal),
-    type
+  const eventName = event == 'snapshot' ? event : 'balance'
+  ctx.eventLogger.emit(eventName, {
+    token_address: token,
+    underlying_decimals: tokenInfo.decimal,
+    token_symbol: tokenInfo.symbol,
+    user_address: user,
+    market: lToken,
+    rate,
+    supply_token: scaleDown(lTokenBalance, 18).multipliedBy(rate),
+    borrow_token: scaleDown(borrowBalance, tokenInfo.decimal),
+    type: event == 'snapshot' ? undefined : event
   })
 }
 
@@ -34,14 +40,14 @@ const markets = await layerbank.allMarkets()
 
 for (const market of markets) {
   const contract = getLtokenContract(EthChainId.SCROLL, market)
-  let info
+  let info, underlying
   try {
-    const underlying = await contract.underlying()
+    underlying = await contract.underlying()
     info = await tokens.getERC20TokenInfo(EthChainId.SCROLL, underlying)
   } catch (e) {
     info = await tokens.getERC20TokenInfo(EthChainId.SCROLL, market)
   }
-  tokenInfos.set(market, info)
+  tokenInfos.set(market, { token: underlying || market, tokenInfo: info })
   console.log('market', market, info.symbol, info.decimal)
 
   LtokenProcessor.bind({ address: market, startBlock, network: EthChainId.SCROLL })
@@ -62,3 +68,32 @@ for (const market of markets) {
       await recordBalance(ctx, market, borrower, 'repay')
     })
 }
+
+LayerbankProcessor.bind({ address, startBlock, network: EthChainId.SCROLL })
+  .onEventMarketEntered(async (evt, ctx) => {
+    const { account, lToken } = evt.args
+    const snap = await ctx.store.get(UserMarkets, account)
+    const markets = snap?.markets || []
+    if (!markets.includes(lToken)) {
+      markets.push(lToken)
+    }
+    await ctx.store.upsert(new UserMarkets({ id: account, markets }))
+  })
+  // .onEventMarketExited(async (evt, ctx) => {
+  //   const { account, lToken } = evt.args
+  //   const snap = await ctx.store.get(UserMarkets, account)
+  //   const markets = (snap?.markets || []).filter((x) => x != lToken)
+  //   await ctx.store.upsert(new UserMarkets({ id: account, markets }))
+  // })
+  .onTimeInterval(
+    async (block, ctx) => {
+      const snaps = await ctx.store.list(UserMarkets, [])
+      for (const { id, markets } of snaps) {
+        if (markets?.length) {
+          await Promise.all(markets.map((market) => recordBalance(ctx, market, id)))
+        }
+      }
+    },
+    60 * 24,
+    60 * 24 * 30
+  )
