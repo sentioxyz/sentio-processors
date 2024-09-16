94f1d5f4f9985ac30c5b246a43c643f1ab0057de
ye <ye@sentio.xyz>
2024-09-16 15:34:18 +0800

    update fuel folder

--- fuel/swaylend-processor/src/constants.ts
+++
@@ -1,6 +0,0 @@
-export const ASSET_ID_TO_SYMBOL: Record<string, string> = {
-  '0xfe744f19db1affff30c628b29d93e330f966ecb649ba83606b1ff49ac9b37f47': 'USDC',
-  '0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07': 'ETH',
-  '0xe71f395845246fd16ddee5317fedaf169c602c13158488979326d5d5e7705719': 'BTC',
-  '0xea49850b5a88ac6291de4be2c11699e65e6a61685da43bef156379ee2764ea94': 'UNI',
-};
