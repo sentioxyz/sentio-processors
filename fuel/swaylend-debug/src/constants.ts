0484949b7cbb7a4ee077565f9182f6690768978c
ye <ye@sentio.xyz>
2024-10-02 15:01:37 +0800

    update sdk and fuel project

---
+++ fuel/swaylend-debug/src/constants.ts
@@ -0,0 +1,9 @@
+export const ASSET_ID_TO_SYMBOL: Record<string, string> = {
+  '0xed6b3b187ea656c16d455b35c33a1469f7ff6a439633293e8eb13ec03c3cf7e5': 'USDC',
+  '0x090b5962feec1b76c71f53cd8eaac7eb2fa48453d382fa04c907f9ca8378c10e': 'USDT',
+  '0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07': 'ETH',
+  '0xe1a1e23b305a27c429af33d22f470b0c9f7f7e45064b5ef79a5e8b587f30b2fe': 'BTC',
+  '0xdc556f50f05d18f73bf9eb832a81c523037e230a3873279b563a0f05c6d5d3cb': 'UNI',
+  '0xe64cd1941eea4de6fa5a1db355f6ccacc555867724e2398066ec671d9efa8e6e': 'BTC',
+  '0xf10411c25a6838c3056f1767f6f07d76ab8c3bef9ff52ef121cb7e3c606e585a': 'BNB',
+};
