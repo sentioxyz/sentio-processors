e7a3d6e39dcc0a4bc7768e93dff3c1dd689a263f
Ping Chen <remotenonsense@gmail.com>
2024-12-17 18:28:51 +0900

    update movement-nft-drop

--- projects/movement-nft-drop/src/processor.ts
+++ projects/movement-nft-drop/src/processor.ts
@@ -1,11 +1,16 @@
-import { collection } from "./types/aptos/movement-mainnet/0x0000000000000000000000000000000000000000000000000000000000000004.js"
-collection.bind()
-.onEventMintEvent(async (evt, ctx) => {
-  ctx.eventLogger.emit('mint', {
-    //@ts-ignore
-    collection: evt.data_decoded.collection,
-    //@ts-ignore
-    id: evt.data_decoded.index.value,
-    token: evt.data_decoded.token
-  })
-})
+import { soulbound_nft } from './types/aptos/movement-mainnet/0x000000000000000000000000000000000000000000000000000000000a550c18.js'
+
+soulbound_nft.bind().onEventNFTMintEvent(
+  async (evt, ctx) => {
+    const { owner, token_id } = evt.data_decoded
+    const transfer = ctx.transaction.events[ctx.eventIndex - 1]
+    ctx.eventLogger.emit('mint', {
+      owner,
+      token_id,
+      object: transfer.data.object
+    })
+  },
+  {
+    allEvents: true
+  }
+)
