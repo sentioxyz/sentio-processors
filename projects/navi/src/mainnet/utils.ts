2d2c696b285e420522c8d45f4657aaef38d542a9
Luren L. <haoran.sde@gmail.com>
2025-01-16 12:36:14 -0500

    update

--- projects/navi/src/mainnet/utils.ts
+++ projects/navi/src/mainnet/utils.ts
@@ -1,14 +1,34 @@
 import { CoinMap } from "./interfaces.js";
 
-export const COIN = ["SUI", "wUSDC", "USDT", "WETH", "CETUS", "VoloSui",
-  "HASUI", "NAVX", "WBTC", "AUSD", "nUSDC", "nbETH", "USDY", "NS", "stBTC", "Deep", "FDUSD"];
+export const COIN = [
+  "SUI",
+  "wUSDC",
+  "USDT",
+  "WETH",
+  "CETUS",
+  "VoloSui",
+  "HASUI",
+  "NAVX",
+  "WBTC",
+  "AUSD",
+  "nUSDC",
+  "nbETH",
+  "USDY",
+  "NS",
+  "stBTC",
+  "DEEP",
+  "FDUSD",
+  "BLUE",
+  "BUCK",
+  "suiUSDT",
+  "stSUI",
+];
 
 export const DECIMAL_RAY = 27;
 export const DEFAULT_COIN_DECIMAL = 9;
 
 export const COIN_MAP: CoinMap = {
-  "0x2::sui::SUI":
-    "SUI",
+  "0x2::sui::SUI": "SUI",
   "0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf::coin::COIN":
     "wUSDC",
   "0xc060006111016b8a020ad5b33834984a437aaa7d3c74c18e09a95d48aceab08c::coin::COIN":
