8677eb4ab64d564846739d206eefb03f5907d056
mark1213866 <mark1213866@gmail.com>
2025-07-31 10:35:41 +0800

    add ika

--- sui/navi/src/mainnet/utils.ts
+++ sui/navi/src/mainnet/utils.ts
@@ -28,6 +28,7 @@ export const COIN = [
   "WAL",
   "HAEDAL",
   "XBTC",
+  "IKA",
 ];
 
 export const DECIMAL_RAY = 27;
@@ -60,7 +61,8 @@ export const DECIMAL_MAP: Record<number, number> = {
   23: 8,
   24: 9,
   25: 9,
-  26: 8, // XBTC - 8 decimals (same as other BTC assets)
+  26: 8,
+  27: 9,
 };
 
 export const COIN_MAP: CoinMap = {
@@ -117,6 +119,8 @@ export const COIN_MAP: CoinMap = {
     "HAEDAL",
   "0x876a4b7bce8aeaef60464c11f4026903e9afacab79b9b142686158aa86560b50::xbtc::XBTC":
     "XBTC",
+  "0x7262fb2f7a3a14c888c438a3cd9b912469a58cf60f367352c46584262e8299aa::ika::IKA":
+    "IKA",
 };
 
 export const SYMBOL_MAP: Record<number, string> = {
@@ -147,6 +151,7 @@ export const SYMBOL_MAP: Record<number, string> = {
   24: "WAL",
   25: "HAEDAL",
   26: "XBTC",
+  27: "IKA",
 };
 
 export const FlashLoanCoins: Record<string, string> = {
@@ -206,5 +211,5 @@ export function getIdBySymbol(coinSymbol: string): number | undefined {
       return Number(id);
     }
   }
-  return undefined; 
+  return undefined;
 }
