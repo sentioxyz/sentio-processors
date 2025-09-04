0f6ae23862bf7ce15e45d52f4b913e779918baf5
Fuyao Zhao <zfy0701@gmail.com>
2025-09-04 17:56:23 +0800

    update deps

--- projects/pyth/src/evm.ts
+++ projects/pyth/src/evm.ts
@@ -29,35 +29,35 @@ const price_update_counter = Counter.register("price_update_counter", {
 
 // https://docs.pyth.network/price-feeds/contract-addresses/evm
 const CHAIN_ADDRESS_MAP = new Map<EthChainId, string>([
-  [EthChainId.ETHEREUM, "0x4305FB66699C3B2702D4d05CF36551390A4c69C6"], //ETH
-  [EthChainId.OPTIMISM, "0xff1a0f4744e8582df1ae09d5611b887b6a12925c"], //Optimism
-  [EthChainId.BINANCE, "0x4D7E825f80bDf85e913E0DD2A2D54927e9dE1594"], //BSC
+  // [EthChainId.ETHEREUM, "0x4305FB66699C3B2702D4d05CF36551390A4c69C6"], //ETH
+  // [EthChainId.OPTIMISM, "0xff1a0f4744e8582df1ae09d5611b887b6a12925c"], //Optimism
+  // [EthChainId.BSC, "0x4D7E825f80bDf85e913E0DD2A2D54927e9dE1594"], //BSC
   // [97, "0xd7308b14bf4008e7c7196ec35610b1427c5702ea"], //BSC testnet
   [EthChainId.POLYGON, "0xff1a0f4744e8582DF1aE09D5611b887B6a12925C"], //Polygon
-  [EthChainId.ARBITRUM, "0xff1a0f4744e8582DF1aE09D5611b887B6a12925C"], //Arbitrum
-  [EthChainId.FANTOM, "0xff1a0f4744e8582DF1aE09D5611b887B6a12925C"], //Fantom
-  [EthChainId.AURORA, "0xF89C7b475821EC3fDC2dC8099032c05c6c0c9AB9"], //Aurora
+  // [EthChainId.ARBITRUM, "0xff1a0f4744e8582DF1aE09D5611b887B6a12925C"], //Arbitrum
+  // [EthChainId.FANTOM, "0xff1a0f4744e8582DF1aE09D5611b887B6a12925C"], //Fantom
+  // [EthChainId.AURORA, "0xF89C7b475821EC3fDC2dC8099032c05c6c0c9AB9"], //Aurora
   [EthChainId.KUCOIN, "0xE0d0e68297772Dd5a1f1D99897c581E2082dbA5B"], //KCC
-  [EthChainId.AVALANCHE, "0x4305FB66699C3B2702D4d05CF36551390A4c69C6"], //Avalanche
-  [EthChainId.CRONOS, "0xE0d0e68297772Dd5a1f1D99897c581E2082dbA5B"], //Cronos
+  // [EthChainId.AVALANCHE, "0x4305FB66699C3B2702D4d05CF36551390A4c69C6"], //Avalanche
+  // [EthChainId.CRONOS, "0xE0d0e68297772Dd5a1f1D99897c581E2082dbA5B"], //Cronos
   [EthChainId.POLYGON_ZKEVM, "0xC5E56d6b40F3e3B5fbfa266bCd35C37426537c65"], // Polygon zkEVM
-  [EthChainId.ZKSYNC_ERA, "0xf087c864AEccFb6A2Bf1Af6A0382B0d0f6c5D834"], // ZKsync
+  // [EthChainId.ZKSYNC_ERA, "0xf087c864AEccFb6A2Bf1Af6A0382B0d0f6c5D834"], // ZKsync
   [EthChainId.CONFLUX, "0xe9d69CdD6Fe41e7B621B4A688C5D1a68cB5c8ADc"], // Conflux
   [EthChainId.MANTLE, "0xA2aa501b19aff244D90cc15a4Cf739D2725B5729"], // mantle
-  [EthChainId.LINEA, "0xA2aa501b19aff244D90cc15a4Cf739D2725B5729"], // linea
-  [EthChainId.BASE, "0x8250f4aF4B972684F7b336503E2D6dFeDeB1487a"], // base
-  [EthChainId.BLAST, "0xA2aa501b19aff244D90cc15a4Cf739D2725B5729"],
+  // [EthChainId.LINEA, "0xA2aa501b19aff244D90cc15a4Cf739D2725B5729"], // linea
+  // [EthChainId.BASE, "0x8250f4aF4B972684F7b336503E2D6dFeDeB1487a"], // base
+  // [EthChainId.BLAST, "0xA2aa501b19aff244D90cc15a4Cf739D2725B5729"],
   [EthChainId.MODE, "0xA2aa501b19aff244D90cc15a4Cf739D2725B5729"]
 ]);
 
 const CHAIN_NATIVE_MAP = new Map<EthChainId, string>([
   [EthChainId.ETHEREUM, "Crypto.ETH/USD"], //ETH
   [EthChainId.OPTIMISM, "Crypto.OP/USD"], //Optimism
-  [EthChainId.BINANCE, "Crypto.BNB/USD"], //BSC
+  [EthChainId.BSC, "Crypto.BNB/USD"], //BSC
   // ["0xd7308b14bf4008e7c7196ec35610b1427c5702ea".toLowerCase(), "Crypto.BNB/USD"], //BSC testnet
   [EthChainId.POLYGON, "Crypto.MATIC/USD"], //Polygon
   [EthChainId.ARBITRUM, "Crypto.ARB/USD"], //Arbitrum
-  [EthChainId.FANTOM, "Crypto.FTM/USD"], //Fantom
+  // [EthChainId.FANTOM, "Crypto.FTM/USD"], //Fantom
   [EthChainId.AURORA, "Crypto.AURORA/USD"], //Aurora
   [EthChainId.KUCOIN, "Crypto.KCS/USD"], //KCC
   [EthChainId.AVALANCHE, "Crypto.AVAX/USD"], //Avalanche
@@ -116,13 +116,13 @@ async function batchPriceUpdate(
   await recordGasUsage("batchPriceUpdate", evt.transactionHash, ctx)
   // other than onblock, also need to track this whenever batchPriceUpdate was triggered
   try {
-    if (ctx.chainId == EthChainId.FANTOM) {
-      const amount = await ctx.contract.provider!.getBalance(
-        ctx.address,
-        ctx.blockNumber
-      );
-      eth_balance.record(ctx, amount);
-    }
+    // if (ctx.chainId == EthChainId.FANTOM) {
+    //   const amount = await ctx.contract.provider!.getBalance(
+    //     ctx.address,
+    //     ctx.blockNumber
+    //   );
+    //   eth_balance.record(ctx, amount);
+    // }
   } catch (e) {
     console.log("chainId" + ctx.chainId);
     console.log("blockNumber" + ctx.blockNumber);
@@ -167,13 +167,13 @@ async function recordGasUsage(evt: string, hash: string, ctx: PythEVMContext) {
 
 async function blockHandler(block: any, ctx: PythEVMContext) {
   try {
-    if (ctx.chainId == EthChainId.FANTOM) {
-      const amount = await ctx.contract.provider!.getBalance(
-        ctx.address,
-        ctx.blockNumber
-      );
-      eth_balance.record(ctx, amount);
-    }
+    // if (ctx.chainId == EthChainId.FANTOM) {
+    //   const amount = await ctx.contract.provider!.getBalance(
+    //     ctx.address,
+    //     ctx.blockNumber
+    //   );
+    //   eth_balance.record(ctx, amount);
+    // }
   } catch (e) {
     console.log("chainId" + ctx.chainId);
     console.log("blockNumber" + ctx.blockNumber);
