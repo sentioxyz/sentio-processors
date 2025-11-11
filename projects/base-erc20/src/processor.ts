fa65486a5cdbe8f5ff630672e0ab4b178b31cfb4
Ping Chen <remotenonsense@gmail.com>
2025-11-11 17:16:42 +0900

    wip

--- projects/base-erc20/src/processor.ts
+++ projects/base-erc20/src/processor.ts
@@ -13,37 +13,50 @@ const tokenCache = new LRUCache<string, TokenInfo>({
   max: 100_000
 })
 
+const erc721Cache = new LRUCache<string, boolean>({
+  max: 100_000
+})
+
 async function getTokenInfo(ctx: ERC20Context) {
-  let info = tokenCache.get(ctx.address)
+  const address = ctx.contract.address
+  let info = tokenCache.get(address)
   if (!info) {
     try {
-      const contract = getERC20ContractOnContext(ctx, ctx.address)
+      const contract = getERC20ContractOnContext(ctx, address)
       const [decimals, symbol] = await Promise.all([await contract.decimals(), contract.symbol()])
       info = {
         decimals,
         symbol
       }
-      tokenCache.set(ctx.address, info)
-    } catch (e) {}
+      tokenCache.set(address, info)
+    } catch (e) {
+      erc721Cache.set(address, true)
+      console.error('xx', e)
+    }
   }
   return info
 }
 
 ERC20Processor.bind({ address: '*', network: EthChainId.BASE }).onEventTransfer(
   async (evt, ctx) => {
+    console.log('- erc721Cache', erc721Cache.size, ctx.address, ctx.contract.address)
+    if (erc721Cache.get(ctx.contract.address)) {
+      return
+    }
     const { from, to, value } = evt.args
-    const tx_from = ctx.transaction?.from.toLowerCase()
     const info = await getTokenInfo(ctx)
     if (!info?.decimals) {
       console.warn(`decimals not found for ${ctx.address}`)
     }
     ctx.eventLogger.emit('tx', {
-      distinctId: from,
-      from,
-      to,
-      value: info?.decimals ? value.scaleDown(info.decimals) : value,
-      tx_from,
-      symbol: info?.symbol
+      distinctId: from.toLowerCase(),
+      from: from.toLowerCase(),
+      to: to.toLowerCase(),
+      value_raw: value.asBigDecimal(),
+      value: info?.decimals ? value.scaleDown(info.decimals) : value.asBigDecimal(),
+      symbol: info?.symbol,
+      tx_from: ctx.transaction?.from.toLowerCase(),
+      tx_to: ctx.transaction?.to?.toLowerCase()
     })
   },
   undefined,
