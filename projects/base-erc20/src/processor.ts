e771164792bc65cb8471e0aeb3e129aef1fa9174
Ping Chen <remotenonsense@gmail.com>
2025-11-11 17:16:42 +0900

    update base-erc20

--- projects/base-erc20/src/processor.ts
+++ projects/base-erc20/src/processor.ts
@@ -3,6 +3,15 @@ import { ERC20Processor } from '@sentio/sdk/eth/builtin'
 import { EthChainId } from '@sentio/sdk/eth'
 import { ERC20Context, getERC20ContractOnContext } from '@sentio/sdk/eth/builtin/erc20'
 import { LRUCache } from 'lru-cache'
+import { GLOBAL_CONFIG } from '@sentio/runtime'
+
+GLOBAL_CONFIG.execution = {
+  ethAbiDecoderConfig: {
+    ...GLOBAL_CONFIG.execution.ethAbiDecoderConfig,
+    enabled: true,
+    skipWhenDecodeFailed: true
+  }
+}
 
 interface TokenInfo {
   decimals: bigint
@@ -14,17 +23,20 @@ const tokenCache = new LRUCache<string, TokenInfo>({
 })
 
 async function getTokenInfo(ctx: ERC20Context) {
-  let info = tokenCache.get(ctx.address)
+  const address = ctx.contract.address
+  let info = tokenCache.get(address)
   if (!info) {
     try {
-      const contract = getERC20ContractOnContext(ctx, ctx.address)
+      const contract = getERC20ContractOnContext(ctx, address)
       const [decimals, symbol] = await Promise.all([await contract.decimals(), contract.symbol()])
       info = {
         decimals,
         symbol
       }
-      tokenCache.set(ctx.address, info)
-    } catch (e) {}
+      tokenCache.set(address, info)
+    } catch (e) {
+      console.error(e)
+    }
   }
   return info
 }
@@ -32,18 +44,19 @@ async function getTokenInfo(ctx: ERC20Context) {
 ERC20Processor.bind({ address: '*', network: EthChainId.BASE }).onEventTransfer(
   async (evt, ctx) => {
     const { from, to, value } = evt.args
-    const tx_from = ctx.transaction?.from.toLowerCase()
     const info = await getTokenInfo(ctx)
     if (!info?.decimals) {
       console.warn(`decimals not found for ${ctx.address}`)
     }
     ctx.eventLogger.emit('tx', {
-      distinctId: from,
-      from,
-      to,
-      value: info?.decimals ? value.scaleDown(info.decimals) : value,
-      tx_from,
-      symbol: info?.symbol
+      distinctId: from.toLowerCase(),
+      from: from.toLowerCase(),
+      to: to.toLowerCase(),
+      value_raw: value.asBigDecimal(),
+      value: info?.decimals ? value.scaleDown(info.decimals) : value.asBigDecimal(),
+      symbol: info?.symbol,
+      tx_from: ctx.transaction?.from.toLowerCase(),
+      tx_to: ctx.transaction?.to?.toLowerCase()
     })
   },
   undefined,
