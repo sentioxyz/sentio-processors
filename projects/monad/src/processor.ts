7ec49691322c86432d258b7c05c002d7f7093ac1
Ping Chen <remotenonsense@gmail.com>
2025-11-26 17:16:53 +0900

    add monad processor

---
+++ projects/monad/src/processor.ts
@@ -0,0 +1,5 @@
+import './tx.js'
+
+import './chainlink.js'
+import './circle.js'
+import './wormhole.js'
---
+++ projects/monad/src/utils.ts
@@ -0,0 +1,5 @@
+import { EthChainId } from '@sentio/chain'
+
+export const network = EthChainId.MONAD_MAINNET
+// export const startBlock = 38055300
+export const startBlock = 0
