e1a673015105dca368e37f42f8413dce9649878e
xtang <xtang@users.noreply.github.com>
2026-01-08 07:23:17 +0800

    achore(coins): keep the code as same as the 2.0v

--- sui/coins/src/processor.ts
+++ sui/coins/src/processor.ts
@@ -3,10 +3,28 @@ import { coin } from "@sentio/sdk/sui/builtin/0x2";
 import { parseMoveType } from '@sentio/sdk/move';
 import { Metadata } from "./schema/store.js";
 
+let native = false
+
 SuiObjectTypeProcessor.bind({
   objectType: coin.CoinMetadata.type()
 }).onObjectChange(async (changes, ctx) => {
   for (const change of changes) {
+    if (native == false) {
+      console.log("insert native token")
+      await ctx.store.upsert(new Metadata({
+        id: '0x2::sui::SUI',
+        coin_type: '0x2::sui::SUI',
+        name: 'SUI',
+        symbol: 'SUI',
+        decimals: 9,
+        description: 'native SUI token',
+        icon_url: undefined,
+        digest: ctx.txDigest,
+        timestamp: 0
+      }))
+      native = true
+    }
+
     if (change.type !== 'created') {
       continue
     }
